b06f77f7c36fceef86d1806d1b98b15bddf52ac8
Olivier Cahagne <wolruf@users.sourceforge.net>
2000-10-11 20:20:14 +0000

    HTML 4.01 Header and charset update

--- html/standalone_top.php
+++ html/standalone_top.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <HTML>
 	<HEAD><TITLE>NOCC Desktop - Webmail</TITLE>
 		<META http-equiv="pragma" content="no-cache">
-		<META content="text/html; charset=windows-1252" http-equiv=Content-Type>
+		<META content="text/html; charset=iso-8859-1" http-equiv="Content-Type">
 		<LINK href="style.css" rel="stylesheet">
 	</HEAD>
 	<BODY alink="<? echo $alink_color?>" background="img/home.gif" bgcolor="#ffffff" link="<? echo $link_color ?>" text="<? echo $text_color ?>" vlink="<? echo $vlink_color ?>">
