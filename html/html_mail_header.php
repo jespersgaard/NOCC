1c3e9baa329dee4346b50219719fac8a94013327
Nicolas Chalanset <nicocha@users.sourceforge.net>
2000-10-22 15:56:17 +0000

    html bugs corrected

--- html/html_mail_header.php
+++ html/html_mail_header.php
@@ -1,40 +1,13 @@
 <?
 if ($verbose == 1 && $use_verbose == true)
-	echo "<TR><TD class='menu'><a 
-href=\"$PHP_SELF?action=aff_mail&mail=$mail&verbose=0&sort=".$sort."&sortdir=".$sortdir."\" 
-class='menu'>".$html_remove_header."</A></TD>";
+	echo "<TR><TD class='menu'><a href=\"$PHP_SELF?action=aff_mail&mail=$mail&verbose=0&sort=".$sort."&sortdir=".$sortdir."\" class='menu'>".$html_remove_header."</A></TD>";
 elseif ($use_verbose == true)
-	echo "<TR><TD class='menu'><a 
-href=\"$PHP_SELF?action=aff_mail&mail=$mail&verbose=1&sort=".$sort."&sortdir=".$sortdir."\" 
-class='menu'>".$html_view_header."</A></TD>";
+	echo "<TR><TD class='menu'><a href=\"$PHP_SELF?action=aff_mail&mail=$mail&verbose=1&sort=".$sort."&sortdir=".$sortdir."\" class='menu'>".$html_view_header."</A></TD>";
 else
 	echo "<TR><TD>&nbsp;</TD>";
+?>	
 
-// here comes the navigation 
 
-/*if (($mail == 1) && ($num_messages > 1))
-{
-	echo "<TD>&nbsp;</TD><TD>&nbsp;</TD><TD>&nbsp;</TD><TD ALIGN=RIGHT colspan=2><a 
-href=\"$PHP_SELF?action=aff_mail&mail=2&verbose=0\"><img src=\"img/next.gif\" 
-border=\"0\"></A></TD></TR>";
-}
-elseif (($mail == $num_messages) && ($mail > 1))
-{
-	$mailp = $mail - 1;
-	echo "<TD>&nbsp;</TD><TD>&nbsp;</TD><TD>&nbsp;</TD><TD ALIGN=RIGHT colspan=2><a 
-href=\"$PHP_SELF?action=aff_mail&mail=$mailp&verbose=0\"><img src=\"img/previous.gif\" 
-border=\"0\"></A></TD></TR>";
-}
-elseif (($mail < $num_messages) && ($mail >= 1))
-{
-	$mailp = $mail - 1;
-	$mailn = $mail + 1;
-	echo "<TD>&nbsp;</TD><TD>&nbsp;</TD><TD>&nbsp;</TD><TD ALIGN=RIGHT colspan=2><a 
-href=\"$PHP_SELF?action=aff_mail&mail=$mailp&verbose=0\"><img src=\"img/previous.gif\" 
-border=\"0\"></A>&nbsp;<a href=\"$PHP_SELF?action=aff_mail&mail=$mailn&verbose=0\"><img 
-src=\"img/next.gif\" border=\"0\"></A></TD></TR>";
-}*/
-?>			
 <td>&nbsp</td></tr>
 <tr><td align="right" class="mail"><? echo $html_from ?></td><td bgcolor="<? echo $html_mail_properties 
 ?>" class="mail"><B><? echo $content["from"] ?></B></td></tr>
