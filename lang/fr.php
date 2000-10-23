b2bffce0fb651fcde8e72f05c6c74b7351b3ca1f
Nicolas Chalanset <nicocha@users.sourceforge.net>
2000-10-23 23:44:42 +0000

    modify colors

--- lang/fr.php
+++ lang/fr.php
@@ -1,16 +1,18 @@
 <?
 /*
-NOCC: Copyright 2000 Nicolas Chalanset <nicocha@free.fr> , Olivier Cahagne <cahagn_o@epita.fr>
+	$File$
+	$Author$
+	$Revision$
+	$Date$
+
+	NOCC: Copyright 2000 Nicolas Chalanset <nicocha@free.fr> , Olivier Cahagne <cahagn_o@epita.fr>
   
   You should have received a copy of the GNU Public
   License along with this package; if not, write to the
   Free Software Foundation, Inc., 59 Temple Place - Suite 330,
   Boston, MA 02111-1307, USA.
-
-Modified by $Author$ ($Date$)
-Revision : $Revision$
-
 */
+
 
 /*
  Configuration file for the french language (the first made, we're french !!)
