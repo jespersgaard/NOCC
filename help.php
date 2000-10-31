400034ee7b5d6cb5c9c81e4375f8b8ad54a0a635
Nicolas Chalanset <nicocha@users.sourceforge.net>
2000-10-31 00:02:58 +0000

    new file handling help system (not finished)

---
+++ help.php
@@ -0,0 +1,16 @@
+<?
+require ("conf.php");
+require ("check_lang.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>NOCC - <? echo $html_help ?></TITLE>
+<META http-equiv="pragma" content="no-cache">
+<META content="text/html; charset=<? echo $charset ?>" http-equiv="Content-Type">
+<LINK href="style.css" rel="stylesheet">
+</HEAD>
+<BODY alink="<? echo $alink_color?>" bgcolor="<? echo $bgcolor ?>" link="<? echo $link_color ?>" text="<? echo $text_color ?>" vlink="<? echo $vlink_color ?>">
+
+</BODY>
+</HTML>
