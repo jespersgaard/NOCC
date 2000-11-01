994e4d05d23e8ca852b427ded6aa63760a99a8fb
Olivier Cahagne <wolruf@users.sourceforge.net>
2000-11-01 11:48:03 +0000

    Optimized HTML images tag with size and alt

--- html/html_bottom_table.php
+++ html/html_bottom_table.php
@@ -1,8 +1,8 @@
 <tr bgcolor=<? echo $html_tr_color ?>>
 	<td align="center" valign="center">
-		<a href="javascript:document.delete_form.submit();"><img src="img/delete.gif" border="0" alt="<? echo $alt_delete ?>"></a>
+		<a href="javascript:document.delete_form.submit();"><img src="img/delete.gif" border="0" height="31" width="29" alt="<? echo $alt_delete ?>"></a>
 	</td>
-	<!--<INPUT TYPE=image SRC="img/delete.gif" ALT="<? echo $alt_delete ?>" BORDER=0></td> -->
+	<!--<INPUT TYPE=image SRC="img/delete.gif" height="31" width="29" ALT="<? echo $alt_delete ?>" BORDER=0></td> -->
 	<td colspan="6">&nbsp;</td>
 </tr>
 </TABLE>
