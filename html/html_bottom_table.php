f9544f5aa6065b936b7b30bebe35ce9520570cfe
Olivier Cahagne <wolruf@users.sourceforge.net>
2000-11-02 13:42:39 +0000

    Forgot to change HTML size of the new trash icon

--- html/html_bottom_table.php
+++ html/html_bottom_table.php
@@ -2,7 +2,7 @@
 	<td align="center" valign="center">
 		<a href="javascript:document.delete_form.submit();"><img src="img/delete.png" border="0" height="32" width="32" alt="<? echo $alt_delete ?>"></a>
 	</td>
-	<!--<INPUT TYPE=image SRC="img/delete.png" height="31" width="29" ALT="<? echo $alt_delete ?>" BORDER=0></td> -->
+	<!--<INPUT TYPE=image SRC="img/delete.png" height="32" width="32" ALT="<? echo $alt_delete ?>" BORDER=0></td> -->
 	<td colspan="6">&nbsp;</td>
 </tr>
 </TABLE>
