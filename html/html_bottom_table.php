94d66ed851643b6e78110bd50fc45b63b0404db4
Nicolas Chalanset <nicocha@users.sourceforge.net>
2000-12-15 21:34:03 +0000

    now javascript + input type=image for the trash button

--- html/html_bottom_table.php
+++ html/html_bottom_table.php
@@ -1,8 +1,8 @@
 <tr bgcolor=<? echo $html_tr_color ?>>
 	<td align="center" valign="center">
-		<a href="javascript:document.delete_form.submit();"><img src="img/delete.png" border="0" height="32" width="32" alt="<? echo $alt_delete ?>"></a>
+		<a href="javascript:document.delete_form.submit();"><INPUT TYPE="image" SRC="img/delete.png" height="32" width="32" ALT="<? echo $alt_delete ?>" BORDER="0" height="32" width="32"></a>
 	</td>
-	<!--<INPUT TYPE=image SRC="img/delete.png" height="32" width="32" ALT="<? echo $alt_delete ?>" BORDER=0></td> -->
+	<!--<INPUT TYPE="image" SRC="img/delete.png" height="32" width="32" ALT="<? echo $alt_delete ?>" BORDER="0"></td> -->
 	<td colspan="6">&nbsp;</td>
 </tr>
 </TABLE>
