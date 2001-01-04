34519670fc61e40064d1f2bf506cb431b4725630
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-01-04 21:40:56 +0000

    Keeping lang pref

--- wrong.php
+++ wrong.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Header$
+ * $Header: /cvsroot/nocc/nocc/webmail/wrong.php,v 1.5 2000/11/24 22:01:52 wolruf Exp $
  *
  * Copyright 2000 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2000 Olivier Cahagne <cahagn_o@epita.fr>
@@ -17,6 +17,6 @@ session_destroy();
 <span class="f">
 <? echo $html_wrong ?>
 <br><br>
-<a href="index.php"><? echo $html_retry ?></a>
+<a href="index.php?lang=<? echo $lang ?>"><? echo $html_retry ?></a>
 </span>
 <br><br><br><br><br>
