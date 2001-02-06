f1723ed71cc8af279f7baad89e1a7e03e2e3e0d9
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-02-06 17:00:24 +0000

    correct a problem with quoted-printable

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/download.php,v 1.15 2001/01/31 00:14:20 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/download.php,v 1.16 2001/02/05 13:22:10 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -20,12 +20,12 @@ if (eregi("MSIE", $HTTP_USER_AGENT) && eregi("5.5", $HTTP_USER_AGENT))
 	header('Content-Disposition: filename='.urldecode($filename));
 else
 	header('Content-Disposition: attachment; filename='.urldecode($filename));
-$pop = imap_open("{".$servr."}INBOX", $user, stripslashes($passwd));
+$pop = imap_open("{".$servr."}".$folder, $user, stripslashes($passwd));
 $file = imap_fetchbody($pop, $mail, $part);
 imap_close($pop);
 if ($transfer == "BASE64")
 	$file = imap_base64($file);
-elseif($transfer == "QUOTED_PRINTABLE")
+elseif($transfer == "QUOTED-PRINTABLE")
 	$file = quoted_printable_decode($file);
 header('Content-Length: '.strlen($file));
 echo ($file);
