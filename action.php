4decc11fa2f0d93047ea216abb9314423f84354b
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-02-08 22:30:22 +0000

    correct an arugment problem

--- action.php
+++ action.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/action.php,v 1.21 2001/01/30 13:27:25 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/action.php,v 1.22 2001/02/06 17:01:39 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -58,7 +58,7 @@ switch ($action)
 		break;
 	case "reply":
 		require ("html/menu_inbox.php");
-		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang);
+		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang, $sort, $sortdir);
 		$mail_to = $content["from"];
 		// Test for Re: in subject, should not be added twice ! 
 		if (!strcasecmp(substr($content["subject"], 0, 2), $html_reply_short))
@@ -73,7 +73,7 @@ switch ($action)
 		break;
 	case "reply_all":
 		require ("html/menu_inbox.php");
-		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang);
+		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang, $sort, $sortdir);
 		$mail_to = get_reply_all($user, $domain, $content["from"], $content["to"], $content["cc"]);
 		if (!strcasecmp(substr($content["subject"], 0, 2), $html_reply_short))
 			$mail_subject = $content["subject"];
@@ -87,7 +87,7 @@ switch ($action)
 		break;
 	case "forward":
 		require ("html/menu_inbox.php");
-		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang);
+		$content = aff_mail($servr, $user, stripslashes($passwd), $folder, $mail, $verbose, $lang, $sort, $sortdir);
 		$mail_subject = $html_forward_short.": ".$content["subject"];
 		$mail_body = $original_msg."\n".$html_from.": ".$content["from"]."\n".$html_to.": ".$content["to"]."\n".$html_sent.": ".$content["date"]."\n".$html_subject.": ".$content["subject"]."\n\n".strip_tags($content["body"], "");
 		// We add the attachments of the original message
