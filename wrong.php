2dbe82ce7a5ce9e387aa1361f7b7a0a34175bd22
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-02-16 16:42:44 +0000

    now xhtml 1.0 compliant

--- wrong.php
+++ wrong.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/wrong.php,v 1.7 2001/01/30 09:45:33 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/wrong.php,v 1.8 2001/02/16 15:33:10 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -16,7 +16,7 @@ session_destroy();
 ?>
 <span class="f">
 <? echo $html_wrong ?>
-<br><br>
+<br /><br />
 <a href="index.php?lang=<? echo $lang ?>"><? echo $html_retry ?></a>
 </span>
 <br /><br /><br /><br /><br />
