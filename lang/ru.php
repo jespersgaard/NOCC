e45f741520ac03d7022b961cde6e771bf85bfaac
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-02-17 11:58:15 +0000

    Added russian language

---
+++ lang/ru.php
@@ -0,0 +1,107 @@
+<?
+/*
+ * $Header: /cvsroot/nocc/nocc/webmail/lang/en.php,v 1.21 2001/02/06 20:47:46 nicocha Exp $ 
+ *
+ * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
+ * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
+ *
+ * See the enclosed file COPYING for license information (GPL).  If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ *
+ * Configuration file for the russian language
+ * Translation by Sergey Frolovithev <serg@spylog.ru>
+ */
+
+$charset = "Windows-1251";
+
+// Configuration for the days and months
+
+// What language to use (Here, english US --> en_US)
+// see '/usr/share/locale/' for more information
+$lang_locale = "ru_RU";
+
+// What format string should we pass to strftime() for messages sent on
+// days other than today?
+$default_date_format = "%Y-%m-%d"; 
+
+// If the local is not implemented on the host, how we display the date
+$no_locale_date_format = "%Y-%m-%d";
+
+// What format string should we pass to strftime() for messages sent
+// today?
+$default_time_format = "%I:%M %p";
+
+
+// Here is the configuration for the HTML
+
+$err_user_empty = "Не введен логин";
+$err_passwd_empty = "Не введен пароль";
+
+
+// html message
+
+$alt_delete = "Удалить выбранные сообщения";
+$alt_delete_one = "Удалить сообщение";
+$alt_new_msg = "Новые сообщения";
+$alt_reply = "Ответить автору";
+$alt_reply_all = "Ответить всем";
+$alt_forward = "Переслать";
+$alt_next = "Следующее";
+$alt_prev = "Предыдущее";
+
+
+// index.php
+
+$html_lang = "Язык";
+$html_welcome = "Добро пожаловать в";
+$html_login = "Логин";
+$html_passwd = "Пароль";
+$html_submit = "Логин";
+$html_help = "Помощь";
+$html_server = "Сервер";
+$html_wrong = "Логин или пароль не верны";
+$html_retry = "Повторить";
+
+// Other pages
+
+$html_view_header = "Просмотреть заголовок письма";
+$html_remove_header = "Убрать заголовок письма";
+$html_inbox = "Входящие";
+$html_new_msg = "Написать";
+$html_reply = "Ответить";
+$html_reply_short = "Re";
+$html_reply_all = "Ответить всем";
+$html_forward = "Переслать";
+$html_forward_short = "Fw";
+$html_delete = "Удалить";
+$html_new = "Новое";
+$html_mark = "Удалить";
+$html_att = "Прикрепленный файл";
+$html_atts = "Прикрепленные файлы";
+$html_att_unknown = "[неизвестно]";
+$html_attach = "Прикрепленный файл";
+$html_attach_forget = "Вы должны присоединить файл до отправки сообщения!";
+$html_attach_delete = "Удалить выбранные";
+$html_from = "От";
+$html_subject = "Тема";
+$html_date = "Дата";
+$html_sent = "Отправить";
+$html_size = "Размер";
+$html_totalsize = "Общий размер";
+$html_kb = "Kb";
+$html_bytes = "bytes";
+$html_filename = "Имя файла";
+$html_to = "Кому";
+$html_cc = "Копия";
+$html_bcc = "Bcc";
+$html_nosubject = "Без темы";
+$html_send = "Отправить";
+$html_cancel = "Отменить";
+$html_no_mail = "Нет сообщения.";
+$html_logout = "Выйти";
+$html_msg = "Сообщение";
+$html_msgs = "Сообщения";
+
+$original_msg = "-- Original Message --";
+$to_empty = "Поле 'Кому' не должно быть пустым !";
+?>
