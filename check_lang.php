b63be35d476f18b5e71ed0e0a18e7ddca87342d8
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-02-19 11:53:26 +0000

    Typo ')'

--- check_lang.php
+++ check_lang.php
@@ -1,6 +1,6 @@
 <?
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/check_lang.php,v 1.8 2000/11/24 22:01:52 wolruf Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/check_lang.php,v 1.9 2001/01/30 09:45:33 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -9,7 +9,7 @@
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
  * Sets the language (default is browser language if it exists else
- * it's $default_lang
+ * it's $default_lang)
  */
 
 if (!ISSET($lang))
