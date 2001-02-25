7088b8b8ee4c7cd28b2c18a9565230ae878284f2
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-02-25 10:48:54 +0000

    Better buildpackage.sh, uses "find" to remove every CVS entry renamed to .sh ro reflect it's a shell script

---
+++ buildpackage.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# buildpackage.sh      Build a NOCC package for release
+#
+# Author:            Olivier Cahagne
+#
+
+if test $# -eq 0
+then
+  echo "Missing first parameter (name of package)"
+  echo "Usage: buildpackage <name of package>"
+  echo "Example: buildpackage nocc-0.9.3"
+  exit 0
+fi
+
+distname=`echo "$1"`
+myname=`basename \`pwd\`` 
+cd .. && cp -rp $myname $distname
+cd $distname
+ find . -name CVS -type d | xargs rm -rf
+ rm -f buildpackage.sh
+cd ..
+tar cf $distname.tar $distname
+rm -f $distname.tar.*
+gzip -9 $distname.tar
+md5sum $distname.tar.gz
+sync; sleep 2
+md5sum $distname.tar.gz
+gzip -t $distname.tar.gz
+echo creating zip archive
+zip -rq9 $distname.zip $distname
+md5sum $distname.zip
+sync; sleep 2
+md5sum $distname.zip
+zip -T $distname.zip
+rm -rf $distname
