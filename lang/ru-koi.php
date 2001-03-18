c57ac7801a37f4129fe1e0bc7809cd09c00a8bce
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-03-18 15:30:07 +0000

    Non corrupted file

--- lang/ru-koi.php
+++ lang/ru-koi.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/lang/rukoi.php,v 1.21 2001/03/01 22:38 Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/lang/ru.php,v 1.6 2001/03/10 23:09:38 wolruf Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -8,8 +8,9 @@
  * See the enclosed file COPYING for license information (GPL).  If you
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
- * Configuration file for the Russian (KOI8-R) language
- * Translation by Anton Jakimov <t0xa@ls2.lv>
+ * Configuration file for the russian (Windows-1251) language
+ * Translation by Sergey Frolovithev <serg@spylog.ru>
+ * Additional translation Anton Jakimov <t0xa@ls2.lv>
  */
 
 $charset = "KOI8-R";
@@ -20,9 +21,14 @@ $charset = "KOI8-R";
 // see '/usr/share/locale/' for more information
 $lang_locale = "ru_RU";
 
+// Text Alignment
+// Can be right-to-left (rtl) which is needed for proper Arabic, Hebrew
+// Or left-to-right (ltr) which is default for most languages
+$lang_dir = "ltr";
+
 // What format string should we pass to strftime() for messages sent on
 // days other than today?
-$default_date_format = "%d.%m.%y";
+$default_date_format = "%d.%m.%Y";
 
 // If the local is not implemented on the host, how we display the date
 $no_locale_date_format = "%d.%m.%Y";
@@ -34,76 +40,76 @@ $default_time_format = "%H:%M";
 
 // Here is the configuration for the HTML
 
-$err_user_empty = "Äðõæèò ðúôâïêàåï.Þý ÿâòðíïêíê ïå þõå òðíó";
-$err_passwd_empty = "Äðõæèò ðúôâïêàåï.Þý ÿâòðíïêíê ïå þõå òðíó";
+$err_user_empty = "îÅ ××ÅÄÅÎ ÌÏÇÉÎ";
+$err_passwd_empty = "îÅ ××ÅÄÅÎ ÐÁÒÏÌØ";
 
 
 // html message
 
-$alt_delete = "Èäâíêæû þý÷ôâïïýå õðð÷üåïêó";
-$alt_delete_one = "Èäâíêæû õðð÷üåïêå";
-$alt_new_msg = "Ïðþýå õðð÷üåïêó";
-$alt_reply = "Ðæþåæêæû âþæðôè";
-$alt_reply_all = "Ðæþåæêæû þõåî";
-$alt_forward = "Òåôåõíâæû";
-$alt_next = "Õíåä.";
-$alt_prev = "Òôåä.";
+$alt_delete = "õÄÁÌÉÔØ ×ÙÂÒÁÎÎÙÅ ÓÏÏÂÝÅÎÉÑ";
+$alt_delete_one = "õÄÁÌÉÔØ ÓÏÏÂÝÅÎÉÅ";
+$alt_new_msg = "îÏ×ÙÅ ÓÏÏÂÝÅÎÉÑ";
+$alt_reply = "ïÔ×ÅÔÉÔØ Á×ÔÏÒÕ";
+$alt_reply_all = "ïÔ×ÅÔÉÔØ ×ÓÅÍ";
+$alt_forward = "ðÅÒÅÓÌÁÔØ";
+$alt_next = "óÌÅÄÕÀÝÅÅ";
+$alt_prev = "ðÒÅÄÙÄÕÝÅÅ";
 
 
 // index.php
 
-$html_lang = "Óÿýì";
-$html_welcome = "Äð÷ôð òðãâíðþâæû ";
-$html_login = "Êîó òðíûÿðþâæåíó";
-$html_passwd = "Òâôðíû";
-$html_submit = "Þéðä";
-$html_help = "Òðîðüû";
-$html_server = "Õåôþåô";
-$html_wrong = "Íðúêï êíê òâôðíû ïåþåôåï";
-$html_retry = "Åüå ôâÿ";
-$html_on = "on";
-$html_theme = "Theme";
+$html_lang = "ñÚÙË";
+$html_welcome = "äÏÂÒÏ ÐÏÖÁÌÏ×ÁÔØ ×";
+$html_login = "éÍÑ";
+$html_passwd = "ðÁÒÏÌØ";
+$html_submit = "÷ÏÊÔÉ";
+$html_help = "ðÏÍÏÝØ";
+$html_server = "óÅÒ×ÅÒ";
+$html_wrong = "ìÏÇÉÎ ÉÌÉ ÐÁÒÏÌØ ÎÅ ×ÅÒÎÙ";
+$html_retry = "÷ÅÒÎÕÔØÓÑ";
+$html_on = "÷ËÌ.";
+$html_theme = "äÉÚÁÊÎ";
 
 // Other pages
 
-$html_view_header = "Òðõîðæôåæû ÿâúðíðþðì";
-$html_remove_header = "Õòôóæâæû ÿâúðíðþðì";
-$html_inbox = "Þéðäóüêå";
-$html_new_msg = "Ïâòêõâæû";
-$html_reply = "Ðæþåæêæû";
+$html_view_header = "ðÒÏÓÍÏÔÒÅÔØ ÚÁÇÏÌÏ×ÏË ÐÉÓØÍÁ";
+$html_remove_header = "õÂÒÁÔØ ÚÁÇÏÌÏ×ÏË ÐÉÓØÍÁ";
+$html_inbox = "÷ÈÏÄÑÝÉÅ";
+$html_new_msg = "îÁÐÉÓÁÔØ";
+$html_reply = "ïÔ×ÅÔÉÔØ";
 $html_reply_short = "Re";
-$html_reply_all = "Ðæþåæêæû þõåî";
-$html_forward = "Òåôåõíâæû";
+$html_reply_all = "ïÔ×ÅÔÉÔØ ×ÓÅÍ";
+$html_forward = "ðÅÒÅÓÌÁÔØ";
 $html_forward_short = "Fw";
-$html_delete = "Èäâíêæû";
-$html_new = "Ïðþðå";
-$html_mark = "Èäâíêæû";
-$html_att = "Òôêíðãåïêå";
-$html_atts = "Òôêíðãåïêó";
-$html_att_unknown = "[ïåêÿþåõæïðå]";
-$html_attach = "Òôêíðãåïêå";
-$html_attach_forget = "Þý äðíãïý äð÷âþêæû òôêíðãåïêå òåôåä æåî ìâì ðæõýíâæû òêõûîð !";
-$html_attach_delete = "Èäâíêæû ðæîåàåïïýå";
-$html_from = "Ðæ";
-$html_subject = "Æåîâ";
-$html_date = "Äâæâ";
-$html_sent = "Ðæòôâþêæû";
-$html_size = "Ôâÿîåô";
-$html_totalsize = "Ð÷üêë ôâÿîåô";
+$html_delete = "õÄÁÌÉÔØ";
+$html_new = "îÏ×ÏÅ";
+$html_mark = "õÄÁÌÉÔØ";
+$html_att = "ðÒÉËÒÅÐÌÅÎÎÙÊ ÆÁÊÌ";
+$html_atts = "ðÒÉËÒÅÐÌÅÎÎÙÅ ÆÁÊÌÙ";
+$html_att_unknown = "[ÎÅÉÚ×ÅÓÔÎÏ]";
+$html_attach = "ðÒÉËÒÅÐÉÔØ ÆÁÊÌ";
+$html_attach_forget = "÷Ù ÄÏÌÖÎÙ ÐÒÉËÒÅÐÉÔØ ÆÁÊÌ ÄÏ ÏÔÐÒÁ×ËÉ ÓÏÏÂÝÅÎÉÑ!";
+$html_attach_delete = "õÄÁÌÉÔØ ×ÙÂÒÁÎÎÙÅ";
+$html_from = "ïÔ";
+$html_subject = "ôÅÍÁ";
+$html_date = "÷ÒÅÍÑ";
+$html_sent = "ïÔÐÒÁ×ÉÔØ";
+$html_size = "òÁÚÍÅÒ";
+$html_totalsize = "ïÂÝÉÊ ÒÁÚÍÅÒ";
 $html_kb = "Kb";
 $html_bytes = "bytes";
-$html_filename = "Filename";
-$html_to = "Ìðîè";
-$html_cc = "Cc";
+$html_filename = "éÍÑ ÆÁÊÌÁ";
+$html_to = "ëÏÍÕ";
+$html_cc = "ëÏÐÉÑ";
 $html_bcc = "Bcc";
-$html_nosubject = "No subject";
-$html_send = "Ðæòôâþêæû";
-$html_cancel = "Ðæîåïâ";
-$html_no_mail = "Ïåæ õðð÷üåïêë.";
-$html_logout = "Þýéðä";
-$html_msg = "Õðð÷üåïêå";
-$html_msgs = "Õðð÷üåïêó";
+$html_nosubject = "no subject";
+$html_send = "ïÔÐÒÁ×ÉÔØ";
+$html_cancel = "ïÔÍÅÎÉÔØ";
+$html_no_mail = "óÏÏÂÝÅÎÉÊ ÎÅÔ";
+$html_logout = "÷ÙÊÔÉ";
+$html_msg = "óÏÏÂÝÅÎÉÅ";
+$html_msgs = "óÏÏÂÝÅÎÉÑ";
 
 $original_msg = "-- Original Message --";
-$to_empty = "Òðíå \"Ìðîè\" ïå äðíãïð ÷ýæû òèõæýî !";
+$to_empty = "ðÏÌÅ \"ëÏÍÕ\" ÎÅ ÄÏÌÖÎÏ ÂÙÔØ ÐÕÓÔÙÍ !";
 ?>
