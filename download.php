af3ac2e19f0b3c492b2db0cfcd168fc81ea27e01
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-03-29 17:48:51 +0000

    ie ssl problem corrected

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/download.php,v 1.17 2001/02/06 17:00:24 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/download.php,v 1.18 2001/02/23 09:31:57 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -11,15 +11,11 @@
  * File for downloading the attachments
  */
 
-session_register ("user", "passwd");
+if (eregi("MSIE", $HTTP_USER_AGENT) || eregi("Internet Explorer", $HTTP_USER_AGENT))
+	session_cache_limiter('public');
+session_start();
 require ("conf.php");
 
-header('Content-Type: application/x-unknown-'.$mime);
-// IE 5.5 is weird, the line is not correct but it works
-if (eregi("MSIE", $HTTP_USER_AGENT) && eregi("5.5", $HTTP_USER_AGENT))
-	header('Content-Disposition: filename='.urldecode($filename));
-else
-	header('Content-Disposition: attachment; filename='.urldecode($filename));
 $pop = imap_open("{".$servr."}".$folder, $user, stripslashes($passwd));
 $file = imap_fetchbody($pop, $mail, $part);
 imap_close($pop);
@@ -27,6 +23,14 @@ if ($transfer == "BASE64")
 	$file = imap_base64($file);
 elseif($transfer == "QUOTED-PRINTABLE")
 	$file = quoted_printable_decode($file);
+
+header('Content-Type: application/x-unknown-' . $mime);
+// IE 5.5 is weird, the line is not correct but it works
+if (eregi("MSIE", $HTTP_USER_AGENT) && eregi("5.5", $HTTP_USER_AGENT))
+	header('Content-Disposition: filename='.urldecode($filename));
+else
+	header('Content-Disposition: attachment; filename='.urldecode($filename));
+
 header('Content-Length: '.strlen($file));
 echo ($file);
 ?>
