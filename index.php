07cca5ed60075abb41d81e639916db8e3badbc5d
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-04-18 09:12:26 +0000

    correct 2 img path

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/index.php,v 1.61 2001/04/07 20:07:03 wolruf Exp $ 
+ * $Header: /cvsroot/nocc/nocc/webmail/index.php,v 1.62 2001/04/17 20:53:51 nicocha Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -17,12 +17,12 @@ Header("Content-type: text/html; Charset=$charset");
 
 if (floor(phpversion()) != 4)
 {
-	echo "<font color=\"red\"><b>You don't seem to be running PHP 4, you need at least PHP 4 to run NOCC.</b></font><br /><br /><div align=\"center\"><img src=\"img/button.png\" width=\"88\" height=\"31\" alt=\"Powered by NOCC\" /></div>";
+	echo "<font color=\"red\"><b>You don't seem to be running PHP 4, you need at least PHP 4 to run NOCC.</b></font><br /><br /><div align=\"center\"><img src=\"themes/standard/img/button.png\" width=\"88\" height=\"31\" alt=\"Powered by NOCC\" /></div>";
 	exit;
 }
 if (!extension_loaded('imap'))
 {
-	echo "<font color=\"red\"><b>The IMAP module does not seem to be installed on this PHP setup, please see NOCC's documentation.</b></font><br /><br /><div align=\"center\"><img src=\"img/button.png\" width=\"88\" height=\"31\" alt=\"Powered by NOCC\" /></div>";
+	echo "<font color=\"red\"><b>The IMAP module does not seem to be installed on this PHP setup, please see NOCC's documentation.</b></font><br /><br /><div align=\"center\"><img src=\"themes/standard/img/button.png\" width=\"88\" height=\"31\" alt=\"Powered by NOCC\" /></div>";
 	exit;
 }
 ?>
