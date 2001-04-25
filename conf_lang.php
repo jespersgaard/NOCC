efbeb59e94fecaf163096db819ccd69d52d119cb
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-04-25 18:13:26 +0000

    removed languages array from conf.php to this file

---
+++ conf_lang.php
@@ -0,0 +1,102 @@
+<?php
+/*
+ * $Header: /cvsroot/nocc/nocc/webmail/conf.php.dist,v 1.45 2001/04/19 16:49:58 nicocha Exp $
+ *
+ * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
+ * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
+ *
+ * See the enclosed file COPYING for license information (GPL).  If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ */
+ 
+ // ################### Language Array  ################### //
+// If you had language files to '/lang' folder, please list them here
+
+// Arabic
+$lang_array[0]->filename = "ar";
+$lang_array[0]->label = "Arabic";
+
+// Chinese
+$lang_array[1]->filename = "zh-tw";
+$lang_array[1]->label = "Chinese (Taiwan)";
+
+// Chinese Simplified
+$lang_array[2]->filename = "zh-gb";
+$lang_array[2]->label = "Chinese Simplified";
+
+// Czech
+$lang_array[3]->filename = "cs";
+$lang_array[3]->label = "Ceský";
+
+// Svenska
+$lang_array[4]->filename = "da";
+$lang_array[4]->label = "Dansk";
+
+// German
+$lang_array[5]->filename = "de";
+$lang_array[5]->label = "Deutsch";
+
+// Dutch
+$lang_array[6]->filename = "nl";
+$lang_array[6]->label = "Dutch";
+
+// English
+$lang_array[7]->filename = "en";
+$lang_array[7]->label = "English";
+
+// Spanish
+$lang_array[8]->filename = "es";
+$lang_array[8]->label = "Espa&ntilde;ol";
+
+// French
+$lang_array[9]->filename = "fr";
+$lang_array[9]->label = "Fran&ccedil;ais";
+
+// Greek
+$lang_array[10]->filename = "el";
+$lang_array[10]->label = "Greek";
+
+// Italiano
+$lang_array[11]->filename = "it";
+$lang_array[11]->label = "Italiano";
+
+// Hebrew
+$lang_array[12]->filename = "he";
+$lang_array[12]->label = "Hebrew";
+
+// Magyar
+$lang_array[13]->filename = "hu";
+$lang_array[13]->label = "Magyar";
+
+// Polish
+$lang_array[14]->filename = "pl";
+$lang_array[14]->label = "Polski";
+
+// Portuguese
+$lang_array[15]->filename = "pt";
+$lang_array[15]->label = "Portug&ecirc;s";
+
+//Russian
+$lang_array[16]->filename = "ru";
+$lang_array[16]->label = "Russian (Win)";
+
+//Russian
+$lang_array[17]->filename = "ru-koi";
+$lang_array[17]->label = "Russian (Unix)";
+
+//Russian
+$lang_array[18]->filename = "ru-iso";
+$lang_array[18]->label = "Russian (ISO)";
+
+// Swedish
+$lang_array[19]->filename = "sv";
+$lang_array[19]->label = "Svenska";
+
+// Slovenian
+$lang_array[20]->filename = "sl";
+$lang_array[20]->label = "Slovensko";
+
+// Slovenski
+$lang_array[21]->filename = "sk";
+$lang_array[21]->label = "Slovensky";
+?>
