ef9afed26f1314ffbb414702341dffdd56dfb576
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-05-27 11:01:07 +0000

    More " -> ' with themes

--- themes/blue/colors.php
+++ themes/blue/colors.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/themes/blue/colors.php,v 1.1 2001/02/26 11:13:43 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/themes/blue/colors.php,v 1.2 2001/04/25 18:17:32 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -12,57 +12,57 @@
 
 // ################### color configuration ################### //
 
-// Color on the pages background - default is "white"
-$glob_theme->bgcolor = "#000080";
+// Color on the pages background - default is 'white'
+$glob_theme->bgcolor = '#000080';
 
-// Color of the login box border - default is "black"
-$glob_theme->login_border = "#FF9900";
+// Color of the login box border - default is 'black'
+$glob_theme->login_border = '#FF9900';
 
 // Inside color of the login box
-$glob_theme->login_box_bgcolor = "#000080";
+$glob_theme->login_box_bgcolor = '#000080';
 
 // Color of the navigation button (inbox, write, answer, etc.)
-// default is "sky blue"
-$glob_theme->menu_color = "#99cc99";
+// default is 'sky blue'
+$glob_theme->menu_color = '#99cc99';
 
 // The same as above when the user is in that part
-// default is "orange"
-$glob_theme->menu_color_on = "#FF9900";
+// default is 'orange'
+$glob_theme->menu_color_on = '#FF9900';
 
 // Mail properties color (to, from, subject, attachments, etc.)
-// default is "sky blue"
-$glob_theme->mail_properties = "#002266";
+// default is 'sky blue'
+$glob_theme->mail_properties = '#002266';
 
 // Color of all the inside border
-// default is "grey"
-$glob_theme->inside_color = "#000080";
+// default is 'grey'
+$glob_theme->inside_color = '#000080';
 
 // Color of the Inbox line and the garbage line and of the outside page
-// default is "dark blue"
-$glob_theme->tr_color = "#002266";
+// default is 'dark blue'
+$glob_theme->tr_color = '#002266';
 
 // color for the highlight of the sorting in the mailbox
-// default is "yellow"
-$glob_theme->sort_color = "#002266";
+// default is 'yellow'
+$glob_theme->sort_color = '#002266';
 
 // Color of the links
-$glob_theme->link_color = "#FF9900";
+$glob_theme->link_color = '#FF9900';
 
 // Color of the text
-$glob_theme->text_color = "#FFFFFF";
+$glob_theme->text_color = '#FFFFFF';
 
 // Color of the Inbox Text display (Delete, new, attachment ...)
-// default is "white"
-$glob_theme->inbox_text_color = "#000080";
+// default is 'white'
+$glob_theme->inbox_text_color = '#000080';
 
-// Color of the Inbox display - default is "white"
-$glob_theme->inbox_color = "#002266";
+// Color of the Inbox display - default is 'white'
+$glob_theme->inbox_color = '#002266';
 
-// Color of the Message display - default is "white"
-$glob_theme->mail_color = "#002266";
+// Color of the Message display - default is 'white'
+$glob_theme->mail_color = '#002266';
 
 // Color of the visited links
-$glob_theme->vlink_color ="#33CC00";
+$glob_theme->vlink_color ='#33CC00';
 
-$glob_theme->alink_color = "#33CC00";
+$glob_theme->alink_color = '#33CC00';
 ?>
--- themes/standard/colors.php
+++ themes/standard/colors.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/themes/standard/colors.php,v 1.1 2001/02/20 17:34:31 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/themes/standard/colors.php,v 1.2 2001/02/23 09:31:58 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -12,57 +12,57 @@
 
 // ################### color configuration ################### //
 
-// Color on the pages background - default is "white"
-$glob_theme->bgcolor = "#ffffff";
+// Color on the pages background - default is 'white'
+$glob_theme->bgcolor = '#ffffff';
 
-// Color of the login box border - default is "black"
-$glob_theme->login_border = "#000000";
+// Color of the login box border - default is 'black'
+$glob_theme->login_border = '#000000';
 
 // Inside color of the login box
-$glob_theme->login_box_bgcolor = "#cccccc";
+$glob_theme->login_box_bgcolor = '#cccccc';
 
 // Color of the navigation button (inbox, write, answer, etc.)
-// default is "sky blue"
-$glob_theme->menu_color = "#99ccff";
+// default is 'sky blue'
+$glob_theme->menu_color = '#99ccff';
 
 // The same as above when the user is in that part
-// default is "orange"
-$glob_theme->menu_color_on = "#FFC061";
+// default is 'orange'
+$glob_theme->menu_color_on = '#FFC061';
 
 // Mail properties color (to, from, subject, attachments, etc.)
-// default is "sky blue"
-$glob_theme->mail_properties = "#99ccff";
+// default is 'sky blue'
+$glob_theme->mail_properties = '#99ccff';
 
 // Color of all the inside border
-// default is "grey"
-$glob_theme->inside_color = "#f0f0f0";
+// default is 'grey'
+$glob_theme->inside_color = '#f0f0f0';
 
 // Color of the Inbox line and the garbage line and of the outside page
-// default is "dark blue"
-$glob_theme->tr_color = "#002266";
+// default is 'dark blue'
+$glob_theme->tr_color = '#002266';
 
 // color for the highlight of the sorting in the mailbox
-// default is "yellow"
-$glob_theme->sort_color = "#ffffcc";
+// default is 'yellow'
+$glob_theme->sort_color = '#ffffcc';
 
 // Color of the links
-$glob_theme->link_color = "#0033cc";
+$glob_theme->link_color = '#0033cc';
 
 // Color of the text
-$glob_theme->text_color = "#000000";
+$glob_theme->text_color = '#000000';
 
 // Color of the Inbox Text display (Delete, new, attachment ...)
-// default is "white"
-$glob_theme->inbox_text_color = "#ffffff";
+// default is 'white'
+$glob_theme->inbox_text_color = '#ffffff';
 
-// Color of the Inbox display - default is "white"
-$glob_theme->inbox_color = "#ffffff";
+// Color of the Inbox display - default is 'white'
+$glob_theme->inbox_color = '#ffffff';
 
-// Color of the Message display - default is "white"
-$glob_theme->mail_color = "#ffffff";
+// Color of the Message display - default is 'white'
+$glob_theme->mail_color = '#ffffff';
 
 // Color of the visited links
-$glob_theme->vlink_color ="#0033cc";
+$glob_theme->vlink_color ='#0033cc';
 
-$glob_theme->alink_color = "#0033cc";
+$glob_theme->alink_color = '#0033cc';
 ?>
