a534c168d700ec4f9d142820c43b8d1ac23a4a9c
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-06-21 15:26:10 +0000

    correct a problem in safe mod which prevented to add attachments

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/logout.php,v 1.16 2001/05/29 08:51:53 nicocha Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/logout.php,v 1.17 2001/06/15 15:16:38 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -13,8 +13,6 @@ require ('conf.php');
 
 session_start();
 $old_theme = $theme;
-$ini_tmpdir = ini_get($upload_tmp_dir);
-$tmpdir = (!empty($ini_tmpdir)) ? $ini_tmpdir : $tmpdir;
 if (isset($attach_array) && is_array($attach_array))
 	while ($tmp = array_shift($attach_array))
 		@unlink($tmpdir.'/'.$tmp->tmp_file);
