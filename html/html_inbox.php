b401b892526116cf49eda5abce18661108856b6e
Ross Golder <rossigee@users.sourceforge.net>
2001-10-17 21:11:17 +0000

    Removed nowrap table attributes, as they cause problems for 800x600 users

--- html/html_inbox.php
+++ html/html_inbox.php
@@ -10,7 +10,7 @@
 	<td align="center">
 		<?php echo $tmp['attach'] ?>
 	</td>
-	<td nowrap="nowrap" class="inbox" align="left">
+	<td class="inbox" align="left">
 		<a href="<?php echo $PHP_SELF ?>?action=aff_mail&amp;mail=<?php echo $tmp['number'] ?>&amp;sort=<?php echo $sort ?>&amp;sortdir=<?php echo $sortdir?>&amp;verbose=0&amp;lang=<?php echo $lang ?>"><?php echo ($tmp['from']? $tmp['from'] : $html_att_unknown) ?></a>
 	</td>
 	<td class="inbox" align="left">
@@ -19,7 +19,7 @@
 	<td class="inbox" align="left">
 		<?php echo $tmp['date'] ?>
 	</td>
-	<td align="right" nowrap="nowrap" class="inbox">
+	<td nowrap="nowrap" class="inbox">
 		<?php echo $tmp['size'] ?> <?php echo $html_kb ?>
 	</td>
 </tr>
--- html/html_top_table.php
+++ html/html_top_table.php
@@ -13,10 +13,10 @@ $is_Imap = is_Imap($servr);
 		<td <?php if (($is_Imap) || ($have_ucb_pop_server)) echo 'colspan="4"'; else echo 'colspan="3"'; ?>align="left" class="titlew">
 			<b><?php echo $folder ?></b>
 		</td>
-		<td align="right" nowrap="nowrap" class="titlew">
+		<td nowrap="nowrap" class="titlew">
 			<?php echo $current_date ?>
 		</td>
-		<td colspan="2" align="right" class="titlew" nowrap="nowrap">
+		<td align="right" class="titlew" nowrap="nowrap">
 			<?php echo $num_msg ?> <?php if ($num_msg == 1) {echo $html_msg;} else {echo $html_msgs;}?>
 		</td>
 	</tr>
@@ -32,28 +32,28 @@ $is_Imap = is_Imap($servr);
 		<td align="center" class="inbox">
 			&nbsp; <?php //echo $html_att ?>
 		</td>
-		<td nowrap="nowrap" align="center" class="inbox" <?php if ($sort == 2) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
+		<td align="center" class="inbox" <?php if ($sort == 2) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
 			<a href="<?php echo $PHP_SELF ?>?sort=2&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<img src="themes/<?php echo $theme ?>/img/<?php echo $arrow ?>.gif" border="0" width="12" height="12" alt="<?php echo $html_sort_by." ".$html_from; ?>" /></a>
 			&nbsp;
 			<a href="<?php echo $PHP_SELF ?>?sort=2&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<?php echo $html_from ?></a>
 		</td>
-		<td nowrap="nowrap" align="center" class="inbox" <?php if ($sort == 3) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
+		<td align="center" class="inbox" <?php if ($sort == 3) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
 			<a href="<?php echo $PHP_SELF ?>?sort=3&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<img src="themes/<?php echo $theme ?>/img/<?php echo $arrow ?>.gif" border="0" width="12" height="12" alt="<?php echo $html_sort_by." ".$html_subject; ?>" /></a>
 			&nbsp;
 			<a href="<?php echo $PHP_SELF ?>?sort=3&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<?php echo $html_subject ?></a>
 		</td>
-		<td nowrap="nowrap" align="center" class="inbox" <?php if ($sort == 1) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
+		<td align="center" class="inbox" <?php if ($sort == 1) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
 			<a href="<?php echo $PHP_SELF ?>?sort=1&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<img src="themes/<?php echo $theme ?>/img/<?php echo $arrow ?>.gif" border="0" width="12" height="12" alt="<?php echo $html_sort_by." ".$html_date; ?>" /></a>
 			&nbsp;
 			<a href="<?php echo $PHP_SELF ?>?sort=1&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<?php echo $html_date ?></a>
 		</td>
-		<td nowrap="nowrap" align="right" class="inbox" <?php if ($sort == 6) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
+		<td align="right" class="inbox" <?php if ($sort == 6) echo 'bgcolor="'.$glob_theme->sort_color.'"' ?>>
 			<a href="<?php echo $PHP_SELF ?>?sort=6&amp;sortdir=<?php echo $new_sortdir ?>&amp;lang=<?php echo $lang ?>">
 			<img src="themes/<?php echo $theme ?>/img/<?php echo $arrow ?>.gif" border="0" width="12" height="12" alt="<?php echo $html_sort_by." ".$html_size; ?>" /></a>
 			&nbsp;
