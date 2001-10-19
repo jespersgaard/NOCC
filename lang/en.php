b50af300a7d7ccf1d3ceb1a785cbc0489b34730d
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-10-19 12:29:23 +0000

    removed the last line which was empty

--- lang/en.php
+++ lang/en.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/lang/en.php,v 1.32 2001/10/17 22:51:44 rossigee Exp $ 
+ * $Header: /cvsroot/nocc/nocc/webmail/lang/en.php,v 1.33 2001/10/18 23:46:19 rossigee Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -130,5 +130,4 @@ $to_empty = 'The \'To\' field must not be empty !';
 // SMTP problems (class_smtp.php)
 $html_smtp_error_no_conn = "Unable to open connection";
 $html_smtp_error_unexpected = "Unexpected response:";
-
 ?>
