f04330fd0a294b7aba9b003363538a8b14e4a921
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-10-20 11:42:12 +0000

    changed indentation

--- prefs.php
+++ prefs.php
@@ -1,152 +1,167 @@
 <?php
-   /**
-    **  prefs.php
-    **
-    **  This contains functions for manipulating user preferences
-    **
-    **  $Id: prefs.php,v 1.2 2001/10/18 19:39:24 rossigee Exp $
-    **/
+/*
+ * $Header: /cvsroot/nocc/nocc/webmail/send.php,v 1.69 2001/10/19 10:34:25 nicocha Exp $
+ *
+ * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
+ * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
+ *
+ * See the enclosed file COPYING for license information (GPL).  If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ */
 
-   if (defined('prefs_php'))
-      return;
-   define('prefs_php', true);
+if (defined('prefs_php'))
+	return;
+define('prefs_php', true);
 
-   global $prefs_are_cached, $prefs_cache;
-   if (!session_is_registered('prefs_are_cached')) {
-      $prefs_are_cached = false;
-      $prefs_cache = array();
-   }
+global $prefs_are_cached, $prefs_cache;
+if (!session_is_registered('prefs_are_cached'))
+{
+	$prefs_are_cached = false;
+    $prefs_cache = array();
+}
 
-   function cachePrefValues($data_dir, $username) {
-       global $prefs_are_cached, $prefs_cache;
+function cachePrefValues($data_dir, $username)
+{
+	global $prefs_are_cached, $prefs_cache;
        
-       if ($prefs_are_cached)
-           return;
+	if ($prefs_are_cached)
+		return;
+	
+	$filename = $data_dir . $username . '.pref';
        
-       $filename = $data_dir . $username . '.pref';
+    if (file_exists($filename))
+	{
+		$file = fopen($filename, 'r');
+
+        /** read in all the preferences **/
+        $highlight_num = 0;
+        while (! feof($file))
+		{
+			$pref = trim(fgets($file, 1024));
+			$equalsAt = strpos($pref, '=');
+            if ($equalsAt > 0)
+			{
+				$Key = substr($pref, 0, $equalsAt);
+				$Value = substr($pref, $equalsAt + 1);
+                if (substr($Key, 0, 9) == 'highlight')
+				{
+					$Key = 'highlight' . $highlight_num;
+					$highlight_num ++;
+                }
+				if ($Value != '')
+					$prefs_cache[$Key] = $Value;
+			}
+		}
+		fclose($file);
+	}
+
+    session_unregister('prefs_cache');
+    session_register('prefs_cache');
        
-       if (file_exists($filename)) {
-           $file = fopen($filename, 'r');
-
-           /** read in all the preferences **/
-           $highlight_num = 0;
-           while (! feof($file)) {
-              $pref = trim(fgets($file, 1024));
-              $equalsAt = strpos($pref, '=');
-              if ($equalsAt > 0) {
-                  $Key = substr($pref, 0, $equalsAt);
-                  $Value = substr($pref, $equalsAt + 1);
-                  if (substr($Key, 0, 9) == 'highlight') {
-                      $Key = 'highlight' . $highlight_num;
-                      $highlight_num ++;
-                  }
-                  if ($Value != '') {
-                      $prefs_cache[$Key] = $Value;
-                  }
-              }
-           }
-           fclose($file);
-
-       }
-
-       session_unregister('prefs_cache');
-       session_register('prefs_cache');
-       
-       $prefs_are_cached = true;
-       session_unregister('prefs_are_cached');
-       session_register('prefs_are_cached');
-   }
+    $prefs_are_cached = true;
+    session_unregister('prefs_are_cached');
+    session_register('prefs_are_cached');
+}
    
    
-   /** returns the value for $string **/
-   function getPref($data_dir, $username, $string) {
-      global $prefs_cache;
+/** returns the value for $string **/
+function getPref($data_dir, $username, $string)
+{
+	global $prefs_cache;
       
-      cachePrefValues($data_dir, $username);
+	cachePrefValues($data_dir, $username);
       
-      if (isset($prefs_cache[$string]))
-          return $prefs_cache[$string];
-      return '';
-   }
+    if (isset($prefs_cache[$string]))
+		return $prefs_cache[$string];
+	return '';
+}
 
 
-   function savePrefValues($data_dir, $username) {
-      global $prefs_cache;
+function savePrefValues($data_dir, $username)
+{
+	global $prefs_cache;
       
-      $file = fopen($data_dir . $username . '.pref', 'w');
-      foreach ($prefs_cache as $Key => $Value) {
-          if (isset($Value)) {
-              fwrite($file, $Key . '=' . $Value . "\n");
-          }
-      }
-      fclose($file);
-   }
+	$file = fopen($data_dir . $username . '.pref', 'w');
+    foreach ($prefs_cache as $Key => $Value)
+	{
+		if (isset($Value))
+			fwrite($file, $Key . '=' . $Value . "\n");
+	}
+	fclose($file);
+}
 
 
-   function removePref($data_dir, $username, $string) {
-      global $prefs_cache;
+function removePref($data_dir, $username, $string)
+{
+	global $prefs_cache;
       
-      cachePrefValues($data_dir, $username);
+    cachePrefValues($data_dir, $username);
       
-      if (isset($prefs_cache[$string])) {
-          unset($prefs_cache[$string]);
-      }
+    if (isset($prefs_cache[$string]))
+		unset($prefs_cache[$string]);
           
-      savePrefValues($data_dir, $username);
-   }
+	savePrefValues($data_dir, $username);
+}
    
-   /** sets the pref, $string, to $set_to **/
-   function setPref($data_dir, $username, $string, $set_to) {
-      global $prefs_cache;
+/** sets the pref, $string, to $set_to **/
+function setPref($data_dir, $username, $string, $set_to)
+{
+	global $prefs_cache;
       
-      cachePrefValues($data_dir, $username);
-      if (isset($prefs_cache[$string]) && $prefs_cache[$string] == $set_to)
-         return;
-      if ($set_to === '') {
-         removePref($data_dir, $username, $string);
-	 return;
-      }
-      $prefs_cache[$string] = $set_to;
-      savePrefValues($data_dir, $username);
-   }
+	cachePrefValues($data_dir, $username);
+    if (isset($prefs_cache[$string]) && $prefs_cache[$string] == $set_to)
+		return;
+	if ($set_to === '')
+	{
+		removePref($data_dir, $username, $string);
+		return;
+	}
+    $prefs_cache[$string] = $set_to;
+    savePrefValues($data_dir, $username);
+}
 
 
-   /** This checks if there is a pref file, if there isn't, it will
-       create it. **/
-   function checkForPrefs($data_dir, $username) {
-      $filename = $data_dir . $username . '.pref';
-      if (!file_exists($filename)) {
-	 $default_pref = $data_dir . 'default_pref';
-         if (file_exists($default_pref)) {
-            if (!copy($default_pref, $filename)) {
-               $file = fopen($data_dir . $username . '.pref', 'w');
-               fclose($file);
-            }
-         }
-      }
-   }
+/** This checks if there is a pref file, if there isn't, it will create it. **/
+function checkForPrefs($data_dir, $username)
+{
+	$filename = $data_dir . $username . '.pref';
+    if (!file_exists($filename))
+	{
+		$default_pref = $data_dir . 'default_pref';
+        if (file_exists($default_pref))
+		{
+			if (!copy($default_pref, $filename))
+			{
+				$file = fopen($data_dir . $username . '.pref', 'w');
+				fclose($file);
+			}
+		}
+	}
+}
 
 
-   /** Writes the Signature **/
-   function setSig($data_dir, $username, $string) {
-      $file = fopen($data_dir . $username . '.sig', 'w');
-      fwrite($file, $string);
-      fclose($file);
-   }
+/** Writes the Signature **/
+function setSig($data_dir, $username, $string)
+{
+	$file = fopen($data_dir . $username . '.sig', 'w');
+    fwrite($file, $string);
+    fclose($file);
+}
 
 
 
-   /** Gets the signature **/
-   function getSig($data_dir, $username) {
-      $filename = $data_dir . $username . '.sig';
-      $sig = '';
-      if (file_exists($filename)) {
-         $file = fopen($filename, 'r');
-         while (!feof($file)) {
-            $sig .= fgets($file, 1024);
-         }
-         fclose($file);
-      }
-      return $sig;
-   }
+/** Gets the signature **/
+function getSig($data_dir, $username)
+{
+	$filename = $data_dir . $username . '.sig';
+	$sig = '';
+    if (file_exists($filename))
+	{
+		$file = fopen($filename, 'r');
+		while (!feof($file))
+			$sig .= fgets($file, 1024);
+		fclose($file);
+	}
+	return $sig;
+}
 ?>
