c0de5944b00d428a49adedc2e6f220b7fa839086
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-10-20 11:48:57 +0000

    reset the align for IE6 and modify some tags placements for XHTML

--- html/prefs.php
+++ html/prefs.php
@@ -4,36 +4,28 @@ if(isset($submit_prefs)) {
 }
 
 ?>
-
-
-<form method="post" action="<?php echo $PHP_SELF; ?>">
-<input type="hidden" name="action" value="setprefs">
-<input type="hidden" name="submit_prefs" value="set">
 <table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
 	<tr>
 		<td bgcolor="<?php echo $glob_theme->inside_color ?>">
+			<form method="post" action="<?php echo $PHP_SELF; ?>">
+			<input type="hidden" name="action" value="setprefs" />
+			<input type="hidden" name="submit_prefs" value="set" />
 			<table border="0" cellpadding="2" cellspacing="1" bgcolor="<?php echo $glob_theme->inside_color ?>" width="100%">
 				<tr>
-					<td class="prefs" valign="top">
-						<?php echo $html_full_name ?>
-					</td>
-					<td class="prefs">
+					<td align="right" class="prefs" valign="top"><?php echo $html_full_name ?> : </td>
+					<td align="left" class="prefs">
 						<input type="text" name="full_name" value="<?php echo $full_name ?>" size="40"/>
 					</td>
 				</tr>
 				<tr>
-					<td class="prefs" valign="top">
-						<?php echo $html_email_address ?>
-					</td>
-					<td class="prefs">
+					<td align="right" class="prefs" valign="top"><?php echo $html_email_address ?> : </td>
+					<td align="left" class="prefs">
 						<input type="text" name="email_address" value="<?php echo $email_address ?>" size="40"/>
 					</td>
 				</tr>
 				<tr>
-					<td class="prefs" valign="top">
-						<?php echo $html_signature ?>
-					</td>
-					<td class="prefs">
+					<td align="right" class="prefs" valign="top"><?php echo $html_signature ?> : </td>
+					<td align="left" class="prefs">
 						<textarea name="signature" rows="5" cols="40"><?php echo $signature ?></textarea>
 					</td>
 				</tr>
@@ -52,7 +44,7 @@ if(isset($submit_prefs)) {
 					</td>
 				</tr>
 			</table>
+			</form>
 		</td>
 	</tr>
 </table>
-</form>
