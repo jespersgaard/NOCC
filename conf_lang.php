2b0a00d6a0ce729b8b947d8faec59c2630e8d4b6
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-10-21 13:38:58 +0000

    Added Bulgarian translation

--- conf_lang.php
+++ conf_lang.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/conf_lang.php,v 1.11 2001/09/08 11:25:24 wolruf Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/conf_lang.php,v 1.12 2001/10/19 10:34:24 nicocha Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -10,117 +10,121 @@
  */
  
 // ################### Language Array  ################### //
-// If you had language files in 'lang/' folder, please list them here
+// If you add language files in 'lang/' folder, please list them here
 
 // Arabic
 $lang_array[0]->filename = 'ar';
 $lang_array[0]->label = 'Arabic';
 
+// Bulgarian
+$lang_array[1]->filename = 'bg';
+$lang_array[1]->label = 'Bulgarian';
+
 // Chinese
-$lang_array[1]->filename = 'zh-tw';
-$lang_array[1]->label = 'Chinese (Taiwan)';
+$lang_array[2]->filename = 'zh-tw';
+$lang_array[2]->label = 'Chinese (Taiwan)';
 
 // Chinese Simplified
-$lang_array[2]->filename = 'zh-gb';
-$lang_array[2]->label = 'Chinese Simplified';
+$lang_array[3]->filename = 'zh-gb';
+$lang_array[3]->label = 'Chinese Simplified';
 
 // Czech
-$lang_array[3]->filename = 'cs';
-$lang_array[3]->label = 'Cesky';
+$lang_array[4]->filename = 'cs';
+$lang_array[4]->label = 'Cesky';
 
 // Svenska
-$lang_array[4]->filename = 'da';
-$lang_array[4]->label = 'Dansk';
+$lang_array[5]->filename = 'da';
+$lang_array[5]->label = 'Dansk';
 
 // German
-$lang_array[5]->filename = 'de';
-$lang_array[5]->label = 'Deutsch';
+$lang_array[6]->filename = 'de';
+$lang_array[6]->label = 'Deutsch';
 
 // Dutch
-$lang_array[6]->filename = 'nl';
-$lang_array[6]->label = 'Dutch';
+$lang_array[7]->filename = 'nl';
+$lang_array[7]->label = 'Dutch';
 
 // English
-$lang_array[7]->filename = 'en';
-$lang_array[7]->label = 'English';
+$lang_array[8]->filename = 'en';
+$lang_array[8]->label = 'English';
 
 // Spanish
-$lang_array[8]->filename = 'es';
-$lang_array[8]->label = 'Espa&ntilde;ol';
+$lang_array[9]->filename = 'es';
+$lang_array[9]->label = 'Espa&ntilde;ol';
 
 // Finnish
-$lang_array[9]->filename = 'fi';
-$lang_array[9]->label = 'Suomi';
+$lang_array[10]->filename = 'fi';
+$lang_array[10]->label = 'Suomi';
 
 // French
-$lang_array[10]->filename = 'fr';
-$lang_array[10]->label = 'Fran&ccedil;ais';
+$lang_array[11]->filename = 'fr';
+$lang_array[11]->label = 'Fran&ccedil;ais';
 
 // Greek
-$lang_array[11]->filename = 'el';
-$lang_array[11]->label = 'Greek';
+$lang_array[12]->filename = 'el';
+$lang_array[12]->label = 'Greek';
 
 // Hebrew
-$lang_array[12]->filename = 'he';
-$lang_array[12]->label = 'Hebrew';
+$lang_array[13]->filename = 'he';
+$lang_array[13]->label = 'Hebrew';
 
 // Italiano
-$lang_array[13]->filename = 'it';
-$lang_array[13]->label = 'Italiano';
+$lang_array[14]->filename = 'it';
+$lang_array[14]->label = 'Italiano';
 
 // Korean
-$lang_array[14]->filename = 'kr';
-$lang_array[14]->label = 'Korean';
+$lang_array[15]->filename = 'kr';
+$lang_array[15]->label = 'Korean';
 
 // Magyar
-$lang_array[15]->filename = 'hu';
-$lang_array[15]->label = 'Magyar';
+$lang_array[16]->filename = 'hu';
+$lang_array[16]->label = 'Magyar';
 
 // Norwegian Bokmal
-$lang_array[16]->filename = 'no';
-$lang_array[16]->label = 'Norsk bokmål';
+$lang_array[17]->filename = 'no';
+$lang_array[17]->label = 'Norsk bokmål';
 
 // Norwegian Nynorsk
-$lang_array[17]->filename = 'nn';
-$lang_array[17]->label = 'Norsk nynorsk';
+$lang_array[18]->filename = 'nn';
+$lang_array[18]->label = 'Norsk nynorsk';
 
 // Polish
-$lang_array[18]->filename = 'pl';
-$lang_array[18]->label = 'Polski';
+$lang_array[19]->filename = 'pl';
+$lang_array[19]->label = 'Polski';
 
 // Portuguese
-$lang_array[19]->filename = 'pt';
-$lang_array[19]->label = 'Portugu&ecirc;s';
+$lang_array[20]->filename = 'pt';
+$lang_array[20]->label = 'Portugu&ecirc;s';
 
 // Portuguese Brazil
-$lang_array[20]->filename = 'pt-br';
-$lang_array[20]->label = 'Portugu&ecirc;s Brasil';
+$lang_array[21]->filename = 'pt-br';
+$lang_array[21]->label = 'Portugu&ecirc;s Brasil';
 
 //Russian
-$lang_array[21]->filename = 'ru';
-$lang_array[21]->label = 'Russian (Win)';
+$lang_array[22]->filename = 'ru';
+$lang_array[22]->label = 'Russian (Win)';
 
 //Russian
-$lang_array[22]->filename = 'ru-koi';
-$lang_array[22]->label = 'Russian (Unix)';
+$lang_array[23]->filename = 'ru-koi';
+$lang_array[23]->label = 'Russian (Unix)';
 
 //Russian
-$lang_array[23]->filename = 'ru-iso';
-$lang_array[23]->label = 'Russian (ISO)';
+$lang_array[24]->filename = 'ru-iso';
+$lang_array[24]->label = 'Russian (ISO)';
 
 // Swedish
-$lang_array[24]->filename = 'sv';
-$lang_array[24]->label = 'Svenska';
+$lang_array[25]->filename = 'sv';
+$lang_array[25]->label = 'Svenska';
 
 // Slovenian
-$lang_array[25]->filename = 'sl';
-$lang_array[25]->label = 'Slovensko';
+$lang_array[26]->filename = 'sl';
+$lang_array[26]->label = 'Slovensko';
 
 // Slovenski
-$lang_array[26]->filename = 'sk';
-$lang_array[26]->label = 'Slovensky';
+$lang_array[27]->filename = 'sk';
+$lang_array[27]->label = 'Slovensky';
 
 // Turkish
-$lang_array[27]->filename = 'tr';
-$lang_array[27]->filename = 'Turkish';
+$lang_array[28]->filename = 'tr';
+$lang_array[28]->filename = 'Turkish';
 ?>
