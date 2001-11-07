831ca47aea8a601d7eb7511ac7fd0f9cdd044b9b
Ross Golder <rossigee@users.sourceforge.net>
2001-11-07 18:08:57 +0000

    Fixed XHTML1 miscompliance

--- html/menu_inbox_opts.php
+++ html/menu_inbox_opts.php
@@ -1,10 +1,10 @@
-<!-- start of $Id: menu_inbox_opts.php,v 1.1 2001/11/03 21:04:16 rossigee Exp $ -->
+<!-- start of $Id: menu_inbox_opts.php,v 1.2 2001/11/04 23:32:35 rossigee Exp $ -->
 <tr>
  <td colspan="7">
 <table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
 	<tr bgcolor="<?php echo $glob_theme->tr_color ?>">
 		<td align="left">
-			<input type="button" value="<?php echo $html_select_all; ?>" onClick="SelectAll()" />
+			<input type="button" value="<?php echo $html_select_all; ?>" onselect="SelectAll()" />
 		</td>
 		<td align="right">
 			<? if ($delete_button_icon) { ?>
@@ -17,4 +17,4 @@
 </table>
  </td>
 </tr>
-<!-- end of $Id: menu_inbox_opts.php,v 1.1 2001/11/03 21:04:16 rossigee Exp $ -->
+<!-- end of $Id: menu_inbox_opts.php,v 1.2 2001/11/04 23:32:35 rossigee Exp $ -->
