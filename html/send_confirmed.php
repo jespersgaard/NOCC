3c89803c5e9e248977ffc1d97d6d6dde4683f31c
Ross Golder <rossigee@users.sourceforge.net>
2001-11-09 13:46:55 +0000

    I can't believe I committed this without checking it (Sorry\!)

--- html/send_confirmed.php
+++ html/send_confirmed.php
@@ -1,9 +1,10 @@
-<!-- start of $Id: send_error.php,v 1.3 2001/10/25 15:22:34 rossigee Exp $ -->
+<!-- start of $Id: send_confirmed.php,v 1.1 2001/11/03 17:10:09 rossigee Exp $ -->
 <p>
  <?php echo $html_send_confirmed; ?>
 </p>
 
 <p>
- <a href="<?php echo $html_return_to_inbox; ?></a>
+ <a href="$PHP_SELF?action.php"><?php echo $html_return_to_inbox; ?></a>
+</p>
 
-<!-- end of $Id: send_error.php,v 1.3 2001/10/25 15:22:34 rossigee Exp $ -->
+<!-- end of $Id: send_confirmed.php,v 1.1 2001/11/03 17:10:09 rossigee Exp $ -->
