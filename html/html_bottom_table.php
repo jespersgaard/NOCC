2a632d363d3f582f68dadf077565620b393b4a65
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-11-16 18:48:43 +0000

    fixed a wrong placement of <form> and </form> tags

--- html/html_bottom_table.php
+++ html/html_bottom_table.php
@@ -1,9 +1,9 @@
-<!-- start of $Id: html_bottom_table.php,v 1.24 2001/11/03 21:04:16 rossigee Exp $ -->
+<!-- start of $Id: html_bottom_table.php,v 1.25 2001/11/04 23:32:35 rossigee Exp $ -->
 </table>
 </td></tr>
-</table>
 <input type="hidden" name="nothing" value="looks_good" />
 </form>
+</table>
 
 <script type="text/javascript">
 <!--
@@ -19,4 +19,4 @@ function SelectAll() {
 </script>
 
 <?php require_once ('./html/menu_inbox.php'); ?>
-<!-- end of $Id: html_bottom_table.php,v 1.24 2001/11/03 21:04:16 rossigee Exp $ -->
+<!-- end of $Id: html_bottom_table.php,v 1.25 2001/11/04 23:32:35 rossigee Exp $ -->
