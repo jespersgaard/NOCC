690828d73062f3a7ae98c83cb9b0e165dd9b854b
Olivier Cahagne <wolruf@users.sourceforge.net>
2001-11-18 22:42:45 +0000

    Removed check for prefs_dir as we allow NOCC to run without it

--- check.php
+++ check.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/check.php,v 1.2 2001/06/21 17:38:48 nicocha Exp $ 
+ * $Header: /cvsroot/nocc/nocc/webmail/check.php,v 1.3 2001/11/03 21:49:15 rossigee Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -22,11 +22,6 @@ if (!extension_loaded('imap'))
 if (empty($tmpdir))
 {
 	echo '<font color="red"><b>"$tmpdir" is not set in "conf.php". NOCC cannot run.</b></font><br /><br /><div align="center"><img src="themes/standard/img/button.png" width="88" height="31" alt="Powered by NOCC" /></div>';
-	exit;
-}
-if (empty($prefs_dir))
-{
-	echo '<font color="red"><b>"$prefs_dir" is not set in "conf.php". NOCC cannot run.</b></font><br /><br /><div align="center"><img src="themes/standard/img/button.png" width="88" height="31" alt="Powered by NOCC" /></div>';
 	exit;
 }
 ?>
