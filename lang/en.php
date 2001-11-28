18da6fb69e25b1a84574bef703468d8bdae844dc
Ross Golder <rossigee@users.sourceforge.net>
2001-11-28 14:28:42 +0000

    Indicate error is SMTP-related

--- lang/en.php
+++ lang/en.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/lang/en.php,v 1.44 2001/11/17 14:45:01 wolruf Exp $ 
+ * $Header: /cvsroot/nocc/nocc/webmail/lang/en.php,v 1.45 2001/11/18 17:53:32 wolruf Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -140,6 +140,6 @@ $original_msg = '-- Original Message --';
 $to_empty = 'The \'To\' field must not be empty !';
 
 // SMTP problems (class_smtp.php)
-$html_smtp_error_no_conn = "Unable to open connection";
-$html_smtp_error_unexpected = "Unexpected response:";
+$html_smtp_error_no_conn = "Unable to open SMTP connection";
+$html_smtp_error_unexpected = "Unexpected SMTP response:";
 ?>
