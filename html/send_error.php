b38022b961f267a41cad3e24642501b5deafac20
Nicolas Chalanset <nicocha@users.sourceforge.net>
2001-12-03 09:57:35 +0000

    added stylesheet rendering for the error message

--- html/send_error.php
+++ html/send_error.php
@@ -1,3 +1,3 @@
-<!-- start of $Id: send_error.php,v 1.3 2001/10/25 15:22:34 rossigee Exp $ -->
-<p><?php echo $html_error_occurred; ?> : <?php echo $ev->getMessage(); ?></p>
-<!-- end of $Id: send_error.php,v 1.3 2001/10/25 15:22:34 rossigee Exp $ -->
+<!-- start of $Id: send_error.php,v 1.4 2001/11/08 13:23:33 rossigee Exp $ -->
+<p class="inbox"><?php echo $html_error_occurred . ' : ' . $ev->getMessage(); ?></p>
+<!-- end of $Id: send_error.php,v 1.4 2001/11/08 13:23:33 rossigee Exp $ -->
