ee33973a3660fe36cdb72a9a89b4264ab35a08c1
Ross Golder <rossigee@users.sourceforge.net>
2002-03-27 09:52:33 +0000

    I can't _believe_ I didn't check this in. :(

---
+++ html/error.php
@@ -0,0 +1,3 @@
+<!-- start of $Id: send_error.php,v 1.5 2001/12/03 09:57:35 nicocha Exp $ -->
+<p class="error"><?php echo $html_error_occurred . ' : ' . $ev->getMessage(); ?></p>
+<!-- end of $Id: send_error.php,v 1.5 2001/12/03 09:57:35 nicocha Exp $ -->
