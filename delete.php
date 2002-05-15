e8fe8ebeb4e25fbc03c7bb85dda8c5ca35b8a1ce
Ross Golder <rossigee@users.sourceforge.net>
2002-05-15 10:07:18 +0000

    Made delete work with 'register_globals=off', and removed some old debugging code.

--- delete.php
+++ delete.php
@@ -1,6 +1,6 @@
 <?php 
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/delete.php,v 1.40 2002/04/19 23:51:38 mrylander Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/delete.php,v 1.41 2002/04/24 15:17:51 rossigee Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -31,36 +31,36 @@ if (Exception::isException($ev)) {
 
 $num_messages = $pop->num_msg();
 
-if (isset($only_one) && ($only_one == 1)) {
-    if ($move_mode) {
+$mail = $_REQUEST['mail'];
+if (isset($_REQUEST['only_one'])) {
+    if (isset($_REQUEST['move_mode'])) {
         if ($target_folder != $folder) {
             $pop->mail_move($mail, $target_folder);
         }
     }
-    if ($copy_mode) {
+    if (isset($_REQUEST['copy_mode'])) {
         if ($target_folder != $folder) {
             $pop->mail_copy($mail, $target_folder);
         }
     }
-    if ($delete_mode || $delete_mode_x) {
+    if (isset($_REQUEST['delete_mode'])) {
         $pop->delete($mail);
     }
 } else {
     for ($i = 1; $i <= $num_messages; $i++) {
 
-        $do_this_one = $HTTP_POST_VARS['msg-' . $i];
-        if ($do_this_one == 'Y') {
-            if ($move_mode) {
+        if (isset($_REQUEST['msg-'.$i])) {
+            if (isset($_REQUEST['move_mode'])) {
                 if ($target_folder != $folder) {
                     $pop->mail_move($i, $target_folder);
                 }
             }
-            if ($copy_mode) {
+            if (isset($_REQUEST['copy_mode'])) {
                 if ($target_folder != $folder) {
                     $pop->mail_copy($i, $target_folder);
                 }
             }
-            if ($delete_mode || $delete_mode_x) {
+            if (isset($_REQUEST['delete_mode'])) {
                 $pop->delete($i);
             }
         }
@@ -73,12 +73,5 @@ $pop->close();
 // TODO: redirect user to next message
 require_once('./proxy.php');
 header('Location: ' . $conf->base_url . "action.php?folder=$folder");
-
-// For debuging
-//reset($HTTP_POST_VARS);
-//while (list($key, $value) = each($HTTP_POST_VARS)) {
-    //echo "$key == $value\n";
-//}
-//echo "</pre>\n";
 
 ?>
