415712c3a81544225aeba7a44a434159c2403ce9
Mike Rylander <mrylander@users.sourceforge.net>
2002-09-10 23:33:57 +0000

    Adding filter support.

---
+++ html/filter_prefs.php
@@ -0,0 +1,144 @@
+<br>
+<table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
+    <tr>
+        <td bgcolor="<?php echo $glob_theme->inside_color ?>">
+            <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <input type="hidden" name="action" value="managefilters" />
+            <input type="hidden" name="do" value="delete" />
+            <table border="0" cellpadding="2" cellspacing="1" bgcolor="<?php echo $glob_theme->inside_color ?>" width="100%">
+                <tr align="center">
+                    <td colspan="4">
+                        <?php echo $html_filter_select ?>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" valign="top" colspan="4">
+                        <?php if ($html_filter_select) { ?>
+                            <input type="submit" class="button" value="<?php echo $html_filter_remove ?>">
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <hr>
+                        <?php } ?>
+            </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" colspan="4">
+                        <?php 
+                            if ($html_filter_select) {
+                                echo "<i>$html_filter_change_tip</i>\n";
+                            }
+                        ?>
+                    </td>
+                </tr>
+                <tr>
+                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                    <input type="hidden" name="action" value="managefilters" />
+                    <input type="hidden" name="do" value="create" />
+                    <td align="right" class="prefs" valign="top" colspan="2">
+                        <select name="thing1">
+                            <option value="-" selected>--Select One--</option>
+                            <option value="BODY"><?php echo $html_filter_body ?></option>
+                            <option value="SUBJECT"><?php echo $html_filter_subject ?></option>
+                            <option value="TO"><?php echo $html_filter_to ?></option>
+                            <option value="FROM"><?php echo $html_filter_from ?></option>
+                            <option value="CC"><?php echo $html_filter_cc ?></option>
+                        </select>
+                    </td>
+                    <td align="center" class="prefs" valign="top">
+                        &nbsp;contains&nbsp;
+                        <td align="left" class="prefs" valign="top">
+                            <input type="text" name="contains1" size="20" max="80">
+                        </td>
+                </tr>
+                <tr>
+                    <td align="right"></td>
+                    <td align="right" class="prefs" valign="top">
+                        <?php echo $html_and ?>&nbsp;
+                        <select name="thing2">
+                            <option value="-" selected><?php echo $html_select_one ?></option>
+                            <option value="BODY"><?php echo $html_filter_body ?></option>
+                            <option value="SUBJECT"><?php echo $html_filter_subject ?></option>
+                            <option value="TO"><?php echo $html_filter_to ?></option>
+                            <option value="FROM"><?php echo $html_filter_from ?></option>
+                            <option value="CC"><?php echo $html_filter_cc ?></option>
+                        </select>
+                    </td>
+                    <td align="center" class="prefs" valign="top">
+                        contains
+                        <td align="left" class="prefs" valign="top">
+                            <input type="text" name="contains2" size="20" max="80">
+                        </td>
+                </tr>
+                <tr>
+                    <td align="right"></td>
+                    <td align="right" class="prefs" valign="top">
+                        <?php echo $html_and ?>&nbsp;
+                        <select name="thing3">
+                            <option value="-" selected>--Select One--</option>
+                            <option value="BODY"><?php echo $html_filter_body ?></option>
+                            <option value="SUBJECT"><?php echo $html_filter_subject ?></option>
+                            <option value="TO"><?php echo $html_filter_to ?></option>
+                            <option value="FROM"><?php echo $html_filter_from ?></option>
+                            <option value="CC"><?php echo $html_filter_cc ?></option>
+                        </select>
+                    </td>
+                    <td align="center" class="prefs" valign="top">
+                        contains
+                        <td align="left" class="prefs" valign="top">
+                            <input type="text" name="contains3" size="20" max="80">
+                        </td>
+                </tr>
+                <tr>
+                    <td colspan="4" class="prefs" align="center">
+                        Filter Name: <input type="text" name="filtername" size="40" max="80">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4" class="prefs" align="center">
+                        Filter Action: 
+                        <input type="radio" name="filter_action" value="MOVE" checked>Move to
+                        <?php echo $filter_move_to ?>
+                        &nbsp;&nbsp;&nbsp;
+                        <input type="radio" name="filter_action" value="DELETE">Delete
+                    </td>
+
+                <tr>
+                    <td align="center" colspan="4">
+                        <table bgcolor="<?php echo $glob_theme->inside_color ?>" width="100%" cellspacing="0" cellpadding="0" border="0">
+                            <tr>
+                                <td align="right" valign="top" width="50%">
+                                    <input type="submit" class="button" value="<?php echo $html_submit ?>" />
+                                    &nbsp;
+                                </td>
+                                <td align="left" valign="top" width="50%">
+                                    &nbsp;
+                                    <input type="reset" class="button" value="<?php echo $html_cancel ?>" />
+                                    </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+                    </form>
+        </td>
+    </tr>
+    <tr>
+        <td align="center" colspan="2">
+            <?php
+                if(Exception::isException($ev))
+                  echo '<p class="prefs">'.$html_error_occurred.' : '.$ev->getMessage().'</p>';
+                elseif(isset($_REQUEST['do']))
+                  echo '<p class="prefs">'.$html_prefs_updated.'</p>';
+            ?>
+        </td>
+    </tr>
+    <?php echo "<tr>\n    <td align=\"right\" bgcolor=\"$glob_theme->inside_color\">\n";
+          echo "        <a href=\"action.php?action=setprefs\" class=\"menu\">$html_preferences</a>&nbsp|&nbsp".
+               "<a href=\"action.php?action=managefolders\" class=\"menu\">$html_manage_folders_link</a>";
+          echo "\n    </td>\n</tr>";
+    ?>
+</table>
---
+++ user_filters.php
@@ -0,0 +1,127 @@
+<?php
+/*
+ * $Header: /cvsroot/nocc/nocc/webmail/user_filters.php,v 1.0 2002/08/30 21:43:45 mrylander Exp $
+ *
+ * Copyright 2002 Mike Rylander <mrylander@mail.com>
+ *
+ * See the enclosed file COPYING for license information (GPL).  If you
+ * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
+ */
+
+require_once 'exception.php';
+
+class NOCCUserFilters {
+	var $key;
+	var $filterset;
+    
+	// Set when preferences have not been commit
+	var $dirty_flag;
+
+	/*
+	 * Default user profile
+	 */
+	function NOCCUserFilters($key) {
+		$this->key = $key;
+        $this->filterset = array();
+		$this->dirty_flag = 1;
+	}
+
+	/*
+	 * Return the current preferences for the given key. Key is
+	 * 'login@domain'. If it cannot be found for any reason, it
+	 * returns a default profile. If it can be found, but not
+	 * read, it returns an exception.
+	 */
+	function read(&$ev) {
+		global $conf;
+
+		/* Open the preferences file */
+		$filename = $conf->prefs_dir . '/' . $this->key . '.filter';
+		if (!file_exists($filename)) {
+			error_log("$filename does not exist");
+			return $this;
+		}
+		$file = fopen($filename, 'r');
+		if(!$file) {
+			$ev = new Exception("Could not open $filename for reading user preferences");
+			return;
+		}
+
+		/* Read in all the preferences */
+		$highlight_num = 0;
+		while(!feof($file))
+		{
+			$line = trim(fgets($file, 1024));
+			$pipeAt = strpos($line, '|');
+			if($pipeAt <= 0) continue;
+
+			$name = substr($line, 0, $pipeAt);
+			$type = substr($line, $pipeAt + 1, 6);
+			$value = substr($line, $pipeAt + 8);
+
+			if(strlen($name) > 0) {
+				$this->filterset[$name][$type] = $value;
+            }
+		}
+		fclose($file);
+ 
+		$this->dirty_flag = 0;
+		return $this;
+	}
+
+	/*
+	 * If need be, write settings to file.
+	 */
+	function commit(&$ev) {
+		global $conf;
+
+		// Do we need to write?
+		if(!$this->dirty_flag) return;
+
+		// Write prefs to file
+		$filename = $conf->prefs_dir . '/' . $this->key . '.filter';
+		if(file_exists($filename) && !is_writable($filename))
+			return (new Exception($html_prefs_file_error));
+		$file = fopen($filename, 'w');
+		if(!$file)
+			return (new Exception($html_prefs_file_error));
+
+        fwrite($file,"super happy filter file\n");
+        foreach($this->filterset as $name => $filter) {
+            foreach($filter as $type => $thing) {
+                if ($type && $thing) {
+                    fwrite($file, $name.'|'.$type.'='.$thing."\n");
+                }
+            }
+        }
+
+        fclose($file);
+
+        $this->dirty_flag = 0;
+	}
+    
+    /*
+     * Create the filter select box for the prefs page
+     */
+    function html_filter_select() {
+        
+        $output = '';
+        $pre = '<select name="filter" width="80" size="5">'."\n";
+        $post = '</select>'."\n";
+        
+        foreach($this->filterset as $name => $filter) {
+            $search = $filter['SEARCH'];
+            $action = $filter['ACTION'];
+            $output .= "\t<option value=\"$name\"><b>$name</b> : &lt;$search -> $action&gt; </option>\n";
+        }
+
+        if ($output) {
+            return $pre.$output.$post;
+        } else {
+            return '';
+        }
+    }
+
+}
+
+?>
