9c0cda11236bee7f26f6b2728f1a08a1275fcd6e
Ross Golder <rossigee@users.sourceforge.net>
2003-02-06 07:30:01 +0000

    Rather than error_log and continue, attempt to create a default '.filter' file if one doesn't already exist (bug #639942). Also fixes exception misreporting bug.

--- user_filters.php
+++ user_filters.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/user_filters.php,v 1.1 2002/09/10 23:33:57 mrylander Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/user_filters.php,v 1.2 2002/12/03 06:37:07 rossigee Exp $
  *
  * Copyright 2002 Mike Rylander <mrylander@mail.com>
  *
@@ -11,118 +11,125 @@
 require_once 'exception.php';
 
 class NOCCUserFilters {
-	var $key;
-	var $filterset;
+        var $key;
+        var $filterset;
     
-	// Set when preferences have not been commit
-	var $dirty_flag;
+        // Set when preferences have not been commit
+        var $dirty_flag;
 
-	/*
-	 * Default user profile
-	 */
-	function NOCCUserFilters($key) {
-		$this->key = $key;
-		$this->filterset = array();
-		$this->dirty_flag = 1;
-	}
+        /*
+         * Default user profile
+         */
+        function NOCCUserFilters($key) {
+                $this->key = $key;
+                $this->filterset = array();
+                $this->dirty_flag = 1;
+        }
 
-	/*
-	 * Return the current preferences for the given key. Key is
-	 * 'login@domain'. If it cannot be found for any reason, it
-	 * returns a default profile. If it can be found, but not
-	 * read, it returns an exception.
-	 */
-	function read($key, &$ev) {
-		global $conf;
+        /*
+         * Return the current preferences for the given key. Key is
+         * 'login@domain'. If it cannot be found for any reason, it
+         * returns a default profile. If it can be found, but not
+         * read, it returns an exception.
+         */
+        function read($key, &$ev) {
+                global $conf;
 
-		$filters = new NOCCUserFilters($key);
+                $filters = new NOCCUserFilters($key);
 
-		/* Open the preferences file */
-		$filename = $conf->prefs_dir . '/' . $key . '.filter';
-		if (!file_exists($filename)) {
-			error_log("$filename does not exist");
-			return $filters;
-		}
-		$file = fopen($filename, 'r');
-		if(!$file) {
-			$ev = new Exception("Could not open $filename for reading user preferences");
-			return;
-		}
+                /* Open the preferences file */
+                $filename = $conf->prefs_dir . '/' . $key . '.filter';
+                if (!file_exists($filename)) {
+                        $filters->dirty_flag = 1;
+                        $filters->commit($ev);
+                        if(Exception::isException($ev))
+                                return;
+                }
+                $file = fopen($filename, 'r');
+                if(!$file) {
+                        $ev = new Exception("Could not open $filename for reading user preferences");
+                        return;
+                }
 
-		/* Read in all the preferences */
-		$highlight_num = 0;
-		while(!feof($file))
-		{
-			$line = trim(fgets($file, 1024));
-			$pipeAt = strpos($line, '|');
-			if($pipeAt <= 0) continue;
+                /* Read in all the preferences */
+                $highlight_num = 0;
+                while(!feof($file))
+                {
+                        $line = trim(fgets($file, 1024));
+                        $pipeAt = strpos($line, '|');
+                        if($pipeAt <= 0) continue;
 
-			$name = substr($line, 0, $pipeAt);
-			$type = substr($line, $pipeAt + 1, 6);
-			$value = substr($line, $pipeAt + 8);
+                        $name = substr($line, 0, $pipeAt);
+                        $type = substr($line, $pipeAt + 1, 6);
+                        $value = substr($line, $pipeAt + 8);
 
-			if(strlen($name) > 0) {
-				$filters->filterset[$name][$type] = $value;
-			}
-		}
-		fclose($file);
+                        if(strlen($name) > 0) {
+                                $filters->filterset[$name][$type] = $value;
+                        }
+                }
+                fclose($file);
  
-		$filters->dirty_flag = 0;
-		return $filters;
-	}
+                $filters->dirty_flag = 0;
+                return $filters;
+        }
 
-	/*
-	 * If need be, write settings to file.
-	 */
-	function commit(&$ev) {
-		global $conf;
+        /*
+         * If need be, write settings to file.
+         */
+        function commit(&$ev) {
+                global $conf;
+		global $html_prefs_file_error;
 
-		// Do we need to write?
-		if(!$this->dirty_flag) return;
+                // Do we need to write?
+                if(!$this->dirty_flag) return;
 
-		// Write prefs to file
-		$filename = $conf->prefs_dir . '/' . $this->key . '.filter';
-		if(file_exists($filename) && !is_writable($filename))
-			return (new Exception($html_prefs_file_error));
-		$file = fopen($filename, 'w');
-		if(!$file)
-			return (new Exception($html_prefs_file_error));
+                // Write prefs to file
+                $filename = $conf->prefs_dir . '/' . $this->key . '.filter';
+                if(file_exists($filename) && !is_writable($filename)) {
+                        $ev = new Exception($html_prefs_file_error);
+                        return;
+                }
+                $file = fopen($filename, 'w');
+                if(!$file) {
+                        $ev = new Exception($html_prefs_file_error);
+                        return;
+                }
 
-		fwrite($file,"super happy filter file\n");
-		foreach($this->filterset as $name => $filter) {
-			foreach($filter as $type => $thing) {
-				if ($type && $thing) {
-					fwrite($file, $name.'|'.$type.'='.$thing."\n");
-				}
-			}
-		}
+                fwrite($file,"super happy filter file\n");
+                foreach($this->filterset as $name => $filter) {
+                        foreach($filter as $type => $thing) {
+                                if ($type && $thing) {
+                                        fwrite($file, $name.'|'.$type.'='.$thing."\n");
+                                }
+                        }
+                }
 
-		fclose($file);
+                fclose($file);
 
-		$this->dirty_flag = 0;
-	}
+                $this->dirty_flag = 0;
+        }
     
-	/*
-	 * Create the filter select box for the prefs page
-	 */
-	function html_filter_select() {
+        /*
+         * Create the filter select box for the prefs page
+         */
+        function html_filter_select() {
         
-		$output = '';
-		$pre = '<select name="filter" width="80" size="5">'."\n";
-		$post = '</select>'."\n";
+                $output = '';
+                $pre = '<select name="filter" width="80" size="5">'."\n";
+                $post = '</select>'."\n";
         
-		foreach($this->filterset as $name => $filter) {
-			$search = $filter['SEARCH'];
-			$action = $filter['ACTION'];
-			$output .= "\t<option value=\"$name\"><b>$name</b> : &lt;$search -> $action&gt; </option>\n";
-		}
+                foreach($this->filterset as $name => $filter) {
+                        $search = $filter['SEARCH'];
+                        $action = $filter['ACTION'];
+                        $output .= "\t<option value=\"$name\"><b>$name</b> : &lt;$search -> $action&gt; </option>\n";
+                }
 
-		if ($output) {
-			return $pre.$output.$post;
-		} else {
-			return '';
-		}
-	}
+                if ($output) {
+                        return $pre.$output.$post;
+                } else {
+                        return '';
+                }
+        }
 
 }
 
