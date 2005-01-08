220f471754ceb07c0a48847305bf12c55d6eb96b
Arnaud Boudou <goddess_skuld@users.sourceforge.net>
2005-01-08 22:12:21 +0000

    Use language file $html_inbox INBOX name when in default inbox, forgot mail menu, it should be OK now (bug #1089729)

--- html/menu_mail.php
+++ html/menu_mail.php
@@ -1,11 +1,11 @@
-<!-- start of $Id: menu_mail.php,v 1.30 2002/06/27 22:17:52 rossigee Exp $ -->
+<!-- start of $Id: menu_mail.php,v 1.31 2003/12/21 15:40:20 goddess_skuld Exp $ -->
 <table border="0" align="center" cellpadding="0" cellspacing="0" width="100%">
     <tr>
         <td bgcolor="<?php echo $glob_theme->inside_color ?>">
             <table border="0" cellpadding="2" cellspacing="1" bgcolor="<?php echo $glob_theme->inside_color ?>" width="100%">
                 <tr>
                     <td class="menu" align="center" bgcolor="<?php echo $glob_theme->menu_color ?>">
-                        <a href="<?php echo $_SERVER['PHP_SELF'] ?>" class="menu"><?php echo $_SESSION['nocc_folder'] ?></a>
+                        <a href="<?php echo $_SERVER['PHP_SELF'] ?>" class="menu"><?php if ($_SESSION['nocc_folder'] != INBOX) { echo $_SESSION['nocc_folder']; } else { echo $html_inbox; } ?></a>
                     </td>
                     <td class="menu" align="center" bgcolor="<?php echo $glob_theme->menu_color ?>">
                         <a href="<?php echo $_SERVER['PHP_SELF'] ?>?action=write" class="menu"><?php echo $html_new_msg ?></a>
@@ -35,4 +35,4 @@
         </td>
     </tr>
 </table>
-<!-- end of $Id: menu_mail.php,v 1.30 2002/06/27 22:17:52 rossigee Exp $ -->
+<!-- end of $Id: menu_mail.php,v 1.31 2003/12/21 15:40:20 goddess_skuld Exp $ -->
