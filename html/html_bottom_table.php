aa8d47c55602e2e8cb3fb63f826d2239e52eac0e
Arnaud Boudou <goddess_skuld@users.sourceforge.net>
2005-08-02 09:32:48 +0000

    Fix regression with "invert selection" button

--- html/html_bottom_table.php
+++ html/html_bottom_table.php
@@ -1,4 +1,4 @@
-<!-- start of $Id: html_bottom_table.php,v 1.31 2004/08/06 13:59:46 goddess_skuld Exp $ -->
+<!-- start of $Id: html_bottom_table.php,v 1.32 2005/08/01 08:11:16 goddess_skuld Exp $ -->
                       </table>
                     </div>
                   </form>
@@ -21,13 +21,14 @@
 <script type="text/javascript">
 <!--
 function SelectAll() {
-  for (var i = 0; i < document.delete_form.elements.length; i++) {
-    if( document.delete_form.elements[i].name.substr( 0, 4 ) == 'msg-') {
-      document.delete_form.elements[i].checked =
-        !(document.delete_form.elements[i].checked);
+  var form = document.getElementById('delete_form');
+  for (var i = 0; i < form.elements.length; i++) {
+    if( form.elements[i].name.substr( 0, 4 ) == 'msg-') {
+      form.elements[i].checked =
+        !(form.elements[i].checked);
     }
   }
 }
 //-->
 </script>
-<!-- end of $Id: html_bottom_table.php,v 1.31 2004/08/06 13:59:46 goddess_skuld Exp $ -->
+<!-- end of $Id: html_bottom_table.php,v 1.32 2005/08/01 08:11:16 goddess_skuld Exp $ -->
