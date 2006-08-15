cc5230c822767a3780c7112e3915988ff8b323ab
Arnaud Boudou <goddess_skuld@users.sourceforge.net>
2006-08-15 14:49:05 +0000

    Improve RSS handling.

--- rss.php
+++ rss.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/rss.php,v 1.7 2006/08/15 10:51:47 goddess_skuld Exp $
+ * $Header: /cvsroot/nocc/nocc/webmail/rss.php,v 1.8 2006/08/15 13:53:49 goddess_skuld Exp $
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -62,57 +62,90 @@
     exit;
   }
 
+  $ts = time();
+  $tz = date('O', $ts);
+  $tz = substr($tz,0,-2).':'.substr($tz,-2);
+  $ts = date('Y-m-d\\TH:i:s',$ts).$tz;
+
   header('Content-Type: application/rss+xml; charset='.$charset);
   echo "<?xml version=\"1.0\" encoding=\"$charset\" ?>\n";
 ?>
   <rdf:RDF
     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    xmlns:dc="http://purl.org/dc/elements/1.1/"
+    xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
     xmlns:admin="http://webns.net/mvcb/"
     xmlns:content="http://purl.org/rss/1.0/modules/content/"
     xmlns="http://purl.org/rss/1.0/">
 
   <channel rdf:about="<?php echo $conf->webmail_url ?>">
-  <title>NOCC Webmail - <?php echo $_SESSION['nocc_folder'] ?></title>
-  <description>Your mailbox</description>
-  <link><?php echo $conf->webmail_url ?></link>
+    <title>NOCC Webmail - <?php echo $_SESSION['nocc_folder'] ?></title>
+    <description>Your mailbox</description>
+    <link><?php echo $conf->webmail_url ?></link>
+    <dc:language><?php echo $_SESSION['nocc_lang'] ?></dc:language>
+    <dc:creator></dc:creator>
+    <dc:rights></dc:rights>
+    <dc:date><?php echo $ts ?></dc:date>
+    <admin:generatorAgent rdf:resource="http://nocc.sourceforge.net/" />
 
-  <admin:generatorAgent rdf:resource="http://nocc.sourceforge.net/" />
+    <sy:updatePeriod>hourly</sy:updatePeriod>
+    <sy:updateFrequency>1</sy:updateFrequency>
+    <sy:updateBase><?php echo $ts ?></sy:updateBase>
 
-<items>
-<rdf:Seq>
+    <items>
+      <rdf:Seq>
 <?php
 while ($tmp = array_shift($tab_mail)) {
 ?>
-<rdf:li rdf:resource="<?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail='.$tmp['number'].'&amp;verbose=0&amp;rss=true' ?>" />
+        <rdf:li rdf:resource="<?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail='.$tmp['number'].'&amp;verbose=0&amp;rss=true' ?>" />
 <?php
 }
 $tab_mail = $tab_mail_bak;
 ?>
-</rdf:Seq>
-</items>
-</channel>
+      </rdf:Seq>
+    </items>
+  </channel>
 
 <?php
 while ($tmp = array_shift($tab_mail)) {
 ?>
-<item rdf:about="<?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail='.$tmp['number'].'&amp;verbose=0&amp;rss=true&amp;nocc_folder='.$_SESSION['nocc_folder'] ?>">
-<title><?php echo htmlspecialchars(htmlentities($tmp['subject'])) ?></title>
-<link><?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail=' . $tmp['number'] . '&amp;verbose=0&amp;rss=true' ?></link>
-<description>
-<![CDATA[
-<?php echo str_replace("themes/", $conf->webmail_url . "themes/", $tmp['attach']) ?> <?php echo $html_size . ": " . $tmp['size'] . " " . $html_kb ?> <br /><br />
-<?php
-  $attach_tab = array();
-  $content = aff_mail($pop, $attach_tab, $tmp['number'], 0, $ev);
-  if (NoccException::isException($ev)) {
-    require ('./html/error.php');
-    exit;
-  }
-  echo substr($content['body'], 0, 200) . '...';  
-?>
-  ]]>
-  </description>
-</item>
+  <item rdf:about="<?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail='.$tmp['number'].'&amp;verbose=0&amp;rss=true&amp;nocc_folder='.$_SESSION['nocc_folder'] ?>">
+    <title><?php echo htmlspecialchars(htmlentities($tmp['subject'])) ?></title>
+    <link><?php echo $conf->webmail_url . 'action.php?action=aff_mail&amp;mail=' . $tmp['number'] . '&amp;verbose=0&amp;rss=true' ?></link>
+    <!--dc:date></dc:date-->
+    <dc:language><?php echo $_SESSION['nocc_lang'] ?></dc:language>
+    <dc:creator><?php echo htmlentities($tmp['from']) ?></dc:creator>
+    <dc:subject>Email</dc:subject>
+    <description>
+      <![CDATA[
+        <?php echo str_replace("themes/", $conf->webmail_url . "themes/", $tmp['attach']) ?> <?php echo $html_size . ": " . $tmp['size'] . " " . $html_kb ?> <br /><br />
+        <?php
+          $attach_tab = array();
+          $content = aff_mail($pop, $attach_tab, $tmp['number'], 0, $ev);
+          if (NoccException::isException($ev)) {
+            require ('./html/error.php');
+            exit;
+          }
+          echo substr($content['body'], 0, 200) . '...';  
+        ?>
+      ]]>
+    </description>
+    <content:encoded>
+      <![CDATA[
+i       <?php echo str_replace("themes/", $conf->webmail_url . "themes/", $tmp['attach']) ?> <?php echo $html_size . ": " . $tmp['size'] . " " . $html_kb ?> <br /><br />
+        <?php
+          $attach_tab = array();
+          $content = aff_mail($pop, $attach_tab, $tmp['number'], 0, $ev);
+          if (NoccException::isException($ev)) {
+            require ('./html/error.php');
+            exit;
+          }
+          //echo htmlentities($content['body']);
+          echo $content['body'];
+        ?>
+      ]]>
+    </content:encoded>
+  </item>
 <?php
   }
 ?>
