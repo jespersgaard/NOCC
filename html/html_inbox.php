38ddd4b4397eb584e41fcb180b32aa898e7e85bd
Arnaud Boudou <goddess_skuld@users.sourceforge.net>
2006-10-05 15:37:25 +0000

    Patch #1571456 : Do htmlspecialchars() after substr().

--- html/html_inbox.php
+++ html/html_inbox.php
@@ -1,4 +1,4 @@
-<!-- start of $Id: html_inbox.php,v 1.48 2005/08/03 17:48:09 goddess_skuld Exp $ -->
+<!-- start of $Id: html_inbox.php,v 1.49 2006/02/26 09:32:53 goddess_skuld Exp $ -->
 <?php
   if (!isset($conf->loaded))
     die('Hacking attempt');
@@ -21,13 +21,13 @@
                           <?php echo $tmp['attach']; ?>
                         </td>
                         <td class="<?php echo $inbox_class ?>">
-                          <?php echo substr(htmlspecialchars(display_address($tmp['to'])), 0, 55); ?>
+                          <?php echo htmlspecialchars(substr(display_address($tmp['to']), 0, 55)); ?>
                         </td>
                         <td class="<?php echo $inbox_class ?>">
                         <?php if(isset($user_prefs->seperate_msg_win) && $user_prefs->seperate_msg_win) { ?>
-                          <a href="javascript:void(0);" onclick="window.open('<?php echo $_SERVER['PHP_SELF'] ?>?action=aff_mail&amp;mail=<?php echo $tmp['number'] ?>&amp;verbose=0&amp;');" title="<?php echo $tmp['subject']? htmlspecialchars($tmp['subject']) : $html_nosubject; ?>"><?php echo $tmp['subject']? substr(htmlspecialchars($tmp['subject']), 0, 55) : $html_nosubject; ?></a>
+                          <a href="javascript:void(0);" onclick="window.open('<?php echo $_SERVER['PHP_SELF'] ?>?action=aff_mail&amp;mail=<?php echo $tmp['number'] ?>&amp;verbose=0&amp;');" title="<?php echo $tmp['subject']? htmlspecialchars($tmp['subject']) : $html_nosubject; ?>"><?php echo $tmp['subject']? htmlspecialchars(substr($tmp['subject'], 0, 55)) : $html_nosubject; ?></a>
                         <?php } else { ?>
-                          <a href="<?php echo $_SERVER['PHP_SELF'] ?>?action=aff_mail&amp;mail=<?php echo $tmp['number'] ?>&amp;verbose=0&amp;" title="<?php echo $tmp['subject']? htmlspecialchars($tmp['subject']) : $html_nosubject; ?>"><?php echo $tmp['subject']? substr(htmlspecialchars($tmp['subject']), 0, 55) : $html_nosubject; ?></a>
+                          <a href="<?php echo $_SERVER['PHP_SELF'] ?>?action=aff_mail&amp;mail=<?php echo $tmp['number'] ?>&amp;verbose=0&amp;" title="<?php echo $tmp['subject']? htmlspecialchars($tmp['subject']) : $html_nosubject; ?>"><?php echo $tmp['subject']? htmlspecialchars(substr($tmp['subject'], 0, 55)) : $html_nosubject; ?></a>
                         <?php } ?>
                         </td>
                         <td class="<?php echo $inbox_class ?>">
@@ -38,4 +38,4 @@
                           <?php echo $tmp['size'] ?> <?php echo $html_kb ?>
                         </td>
                       </tr>
-<!-- end of $Id: html_inbox.php,v 1.48 2005/08/03 17:48:09 goddess_skuld Exp $ -->
+<!-- end of $Id: html_inbox.php,v 1.49 2006/02/26 09:32:53 goddess_skuld Exp $ -->
