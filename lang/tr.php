85381b86cd5c68286c7d629506d1cf0d5c08e841
Arnaud Boudou <goddess_skuld@users.sourceforge.net>
2008-05-01 07:53:07 +0000

    Patch #1955195 : Turkish Translation complete.

--- lang/tr.php
+++ lang/tr.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * $Header: /cvsroot/nocc/nocc/webmail/lang/tr.php,v 1.42 2008/02/09 12:35:59 goddess_skuld Exp $ 
+ * $Header: /cvsroot/nocc/nocc/webmail/lang/tr.php,v 1.41 2006/10/20 12:20:00 goddess_skuld Exp $ 
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -9,7 +9,9 @@
  * did not receive this file, see http://www.fsf.org/copyleft/gpl.html.
  *
  * Configuration file for the turkish language
- * Missing translation author
+ * Translation by
+ * 	İsmail Özer <aceleyolla AT gmx DOT net>
+ *
  */
 
 $charset = 'UTF-8';
@@ -39,29 +41,29 @@ $default_time_format = '%I:%M %p';
 
 // Here is the configuration for the HTML
 
-$err_user_empty = 'The login field is empty';
+$err_user_empty = 'Kullanıcı kısmını boş geçemezsiniz';
 $err_passwd_empty = 'Parola kısmını boş geçemezsiniz';
 
 
 // html message
-$alt_delete = 'Seçili mesajları sil';
-$alt_delete_one = 'Seçili mesajı sil';
-$alt_new_msg = 'Yeni mesaj';
+$alt_delete = 'Seçili iletileri sil';
+$alt_delete_one = 'Seçili iletiyi sil';
+$alt_new_msg = 'Yeni ileti';
 $alt_reply = 'Yazana cevapla';
 $alt_reply_all = 'Tümüne cevapla';
-$alt_forward = 'İlet';
-$alt_next = 'Next'; //to translate
-$alt_prev = 'Previous'; //to translate
-$title_next_page = 'Next page'; //to translate
-$title_prev_page = 'Previous page'; //to translate
-$title_next_msg = 'Sonraki mesaj';
-$title_prev_msg = 'Önceki Mesaj';
-$html_on = 'on';  //to translate
+$alt_forward = 'Başkasına İlet';
+$alt_next = 'İleri';
+$alt_prev = 'Geri'; 
+$title_next_page = 'Sonraki Sayfa';
+$title_prev_page = 'Önceki Sayfa'; 
+$title_next_msg = 'Sonraki İleti';
+$title_prev_msg = 'Önceki İleti';
+$html_on = 'HTML etkin kıl';
 $html_theme = 'Biçim';
 
 // index.php
 $html_lang = 'Dil';
-$html_welcome = 'Welcome to';  //to translate
+$html_welcome = 'Atabarı Web Mailine Hoş Geldiniz ';
 $html_login = 'Kullanıcı Adı';
 $html_passwd = 'Parola';
 $html_submit = 'Giriş';
@@ -69,177 +71,177 @@ $html_help = 'Yardım';
 $html_server = 'Sunucu';
 $html_wrong = 'Kullanıcı adı ya da parola hatalı';
 $html_retry = 'Tekrar Deneyin';
-$html_remember = "Remember settings"; //to translate
+$html_remember = "Ayarları Hatırla"; 
 
 // prefs.php
-$html_msgperpage = 'Messages per page';  //to translate
-$html_preferences = 'Preferences';  //to translate
-$html_full_name = 'Full name';  //to translate
-$html_email_address = 'E-mail Address';  //to translate
-$html_ccself = 'Cc self';  //to translate
-$html_hide_addresses = 'Hide addresses';  //to translate
-$html_outlook_quoting = 'Outlook-style quoting';  //to translate
-$html_reply_to = 'Reply to';  //to translate
-$html_use_signature = 'Use signature';  //to translate
-$html_signature = 'Signature';  //to translate
-$html_reply_leadin = 'Reply Leadin';  //to translate
-$html_prefs_updated = 'Preferences updated';  //to translate
-$html_manage_folders_link = 'Manage IMAP Folders';  //to translate
-$html_manage_filters_link = 'Manage Email Filters';  //to translate
-$html_use_graphical_smilies = 'Use graphical smilies'; //to translate
-$html_sent_folder = 'Copy sent mails into a dedicated folder'; //to translate
+$html_msgperpage = 'Her sayfada messaj sayısı';
+$html_preferences = 'Ayarlar';
+$html_full_name = 'İsim ve Soyisim';
+$html_email_address = 'E-mail adresi';
+$html_ccself = 'Karbon kopya'; 
+$html_hide_addresses = 'Adresleri gizli tut';  
+$html_outlook_quoting = 'Outlook biçimi işaretle'; 
+$html_reply_to = 'Cevapla';
+$html_use_signature = 'İmza kullan';
+$html_signature = 'İmza';  
+$html_reply_leadin = 'Yanıt giriş';
+$html_prefs_updated = 'Ayarlar güncelleşti';
+$html_manage_folders_link = 'IMAP dosyasını yönet'; 
+$html_manage_filters_link = 'Email süzgecini yönet';
+$html_use_graphical_smilies = 'Grafik ifadelere izin ver';
+$html_sent_folder = 'Gönderilen iletileri ayrı klasöre kopyala';
 $html_trash_folder = 'Move deleted mails into a dedicated folder'; // to translate
-$html_colored_quotes = 'Colored quotes'; //to translate
-$html_display_struct = 'Display structured text'; //to translate
-$html_send_html_mail = 'Send mail in HTML format'; //to translate
+$html_colored_quotes = 'Renkli tırnak işareti kullan'; 
+$html_display_struct = 'Biçimli yazıları göster'; 
+$html_send_html_mail = 'İletiyi HTML formatında gönder'; 
 
 // folders.php
 $html_folders = 'Folders';  //to translate
-$html_folders_create_failed = 'Folder could not be created!';  //to translate
-$html_folders_sub_failed = 'Could not subscribed to folder!';  //to translate
-$html_folders_unsub_failed = 'Could not unsubscribed from folder!';  //to translate
-$html_folders_rename_failed = 'Folder could not be renamed!';  //to translate
-$html_folders_updated = 'Folders updated';  //to translate
-$html_folder_subscribe = 'Subscribe to';  //to translate
-$html_folder_rename = 'Rename';  //to translate
-$html_folder_create = 'Create new folder called';  //to translate
-$html_folder_remove = 'Unsubscribe from';  //to translate
-$html_folder_delete = 'Delete';  //to translate
-$html_folder_to = 'to'; //to translate
+$html_folders_create_failed = 'Klasör oluşturmada bir hata çıktı!';  
+$html_folders_sub_failed = 'Klasörü paylaşmada hata oluştu!';
+$html_folders_unsub_failed = 'Klasörün paylaşımı kaldırlamadı!';
+$html_folders_rename_failed = 'Klasör farklı adlandırlamadı!'; 
+$html_folders_updated = 'Klasörler güncelleştirildi'; 
+$html_folder_subscribe = 'Klasörü paylaş'; 
+$html_folder_rename = 'Farklı adlandır';  
+$html_folder_create = 'Yeni klasörü oluştur';
+$html_folder_remove = 'Klasörü kapat';
+$html_folder_delete = 'Klasörü sil';
+$html_folder_to = 'Yeni adı'; 
 
 // filters.php
-$html_filter_remove = 'Delete';  //to translate
-$html_filter_body = 'Message Body';  //to translate
-$html_filter_subject = 'Message Subject';  //to translate
-$html_filter_to = 'To Field';  //to translate
-$html_filter_cc = 'Cc Field';  //to translate
-$html_filter_from = 'From Field';  //to translate
-$html_filter_change_tip = 'To change a filter simply overwrite it.';  //to translate
-$html_reapply_filters = 'Reapply all filters';  //to translate
-$html_filter_contains = 'contains';  //to translate
-$html_filter_name = 'Filter Name';  //to translate
-$html_filter_action = 'Filter Action';  //to translate
-$html_filter_moveto = 'Move to';  //to translate
+$html_filter_remove = 'Sil';  
+$html_filter_body = 'İleti metni';
+$html_filter_subject = 'İleti başlığı'; 
+$html_filter_to = 'Kime (To)'; 
+$html_filter_cc = 'Kopya (Cc)';
+$html_filter_from = 'Gönderen';
+$html_filter_change_tip = 'İleti süzgecini değiştirmek için varolanı düzenle.';
+$html_reapply_filters = 'Tüm süzgeçleri yeniden çalıştır';
+$html_filter_contains = 'Süzgecin içeriği';  
+$html_filter_name = 'Süzgeç adı'; 
+$html_filter_action = 'Süzgecin işlevi '; 
+$html_filter_moveto = 'Buraya taşı';  
 
 // Other pages
-$html_select_one = '--Select One--';  //to translate
-$html_and = 'And';  //to translate
-$html_new_msg_in = 'New messages in';  //to translate
-$html_or = 'or';  //to translate
-$html_move = 'Move';  //to translate
-$html_copy = 'Copy';  //to translate
-$html_messages_to = 'selected messages to';  //to translate
-$html_gotopage = 'Go to Page';  //to translate
-$html_gotofolder = 'Go to Folder';  //to translate
-$html_other_folders = 'Folder List';  //to translate
-$html_page = 'Page';  //to translate
-$html_of = 'of';  //to translate
+$html_select_one = '-- Seç --'; 
+$html_and = 'Ve';  
+$html_new_msg_in = 'deki yeni iletiyi';
+$html_or = 'Veya';  
+$html_move = 'Taşı';  
+$html_copy = 'Kopyala';  
+$html_messages_to = 'seçilen iletiyi buraya '; 
+$html_gotopage = 'Sayfaya git';  
+$html_gotofolder = 'Klasöre git';
+$html_other_folders = 'Klasör listesi';
+$html_page = 'Sayfa'; 
+$html_of = 'dan';  
 $html_view_header = 'Başlıkları Göster';
 $html_remove_header = 'Başlıkları Gizle';
 $html_inbox = 'Gelen Kutusu';
-$html_new_msg = 'Yeni Mesaj';
+$html_new_msg = 'Yeni İleti';
 $html_reply = 'Cevapla';
 $html_reply_short = 'Ynt';
 $html_reply_all = 'Tümüne cevapla';
 $html_forward = 'İlet';
 $html_forward_short = 'İlt';
-$html_forward_info = 'The forwarded message will be send as an attachment to this one.'; //to translate
+$html_forward_info = 'İletmek istenilen mesaj bu iletinin içinde ek olarak gönderilecektir.'; 
 $html_delete = 'Sil';
 $html_new = 'Yeni';
 $html_mark = 'Sil';
 $html_att = 'Ek';
 $html_atts = 'Ekler';
 $html_att_unknown = '[bilinmiyor]';
-$html_attach = 'Ek';
-$html_attach_forget = 'Mesajı göndermeden önce dosyayı eklemelisiniz !';
+$html_attach = 'Ekle';
+$html_attach_forget = 'İletiyi göndermeden önce dosyayı eklemelisiniz !';
 $html_attach_delete = 'İşaretleneni Sil';
-$html_attach_none = 'You must select a file to attach!';  //to translate
-$html_sort_by = 'Sort by';  //to translate
-$html_sort = 'Sort'; //to translate
+$html_attach_none = 'İleti eki için bir dosya seçmelisiniz!';
+$html_sort_by = 'Göre sırala'; 
+$html_sort = 'Sırala';
 $html_from = 'Gönderen';
 $html_subject = 'Konu';
 $html_date = 'Tarih';
 $html_sent = 'Gönder';
-$html_wrote = 'wrote'; //to translate
-$html_size = 'Boyut';
+$html_wrote = 'Yazdı'; 
+$html_size = 'Boyut ';
 $html_totalsize = 'Toplam Boyut';
-$html_kb = 'kB'; //to translate
-$html_bytes = 'bytes'; //to translate
+$html_kb = ' KB'; 
+$html_bytes = ' Bytes';
 $html_filename = 'Dosya Adı';
 $html_to = 'Alıcı';
-$html_cc = 'Cc';  //to translate
-$html_bcc = 'Bcc';  //to translate
+$html_cc = 'Kopya (Cc)';
+$html_bcc = 'Gizli Kopya (Bcc)'; 
 $html_nosubject = 'Konusuz';
 $html_send = 'Gönder';
 $html_cancel = 'İptal';
-$html_no_mail = 'Mesaj Yok.';
+$html_no_mail = 'İleti Yok.';
 $html_logout = 'Çıkış';
-$html_msg = 'Mesaj';
-$html_msgs = 'Mesajlar';
-$html_configuration = 'Bu sunucusu henüz konfigure edilmemiş !';
-$html_priority = 'Priority';  //to translate
-$html_low = 'Low';  //to translate
-$html_normal = 'Normal';  //to translate
-$html_high = 'High';  //to translate
-$html_receipt = 'Request a return receipt';  //to translate
-$html_select = 'Select';  //to translate
-$html_select_all = 'Invert Selection';  //to translate
-$html_loading_image = 'Loading image';  //to translate
-$html_send_confirmed = 'Your mail was accepted for delivery';  //to translate
-$html_no_sendaction = 'No action specified. Try enabling JavaScript.';  //to translate
-$html_error_occurred = 'An error occurred';  //to translate
-$html_prefs_file_error = 'Unable to open preferences file for writing.';  //to translate
-$html_wrap = 'Wrap outgoing messages to :';  //to translate
-$html_wrap_none = 'None'; //to translate
-$html_usenet_separator = 'Usenet separator ("-- \n") Before signature';  //to translate
-$html_mark_as = 'Mark as'; //to translate
-$html_read = 'read'; //to translate
-$html_unread = 'unread'; //to translate
-$html_encoding = 'Character encoding'; // to translate
+$html_msg = 'İleti';
+$html_msgs = 'İletiler';
+$html_configuration = 'Bu sunucusu henüz ayarlanmamış!';
+$html_priority = 'İletinin önemliliği'; 
+$html_low = 'Düşük'; 
+$html_normal = 'Orta'; 
+$html_high = 'Yüksek';  
+$html_receipt = 'İleti okundu onayı gönderilsin';
+$html_select = 'Seç';
+$html_select_all = 'Ters seçim yap';
+$html_loading_image = 'Resim yükle';  
+$html_send_confirmed = 'İletiniz başarı ile gönderildi';
+$html_no_sendaction = 'Bir aksiyon şey seçilmedi. JavaScriptini seçin ve tekrar deneyin.';
+$html_error_occurred = 'Bir teknik hata oluştu'; 
+$html_prefs_file_error = 'Hata: Ayarlar dosyası yazmak için açılamadı.'; 
+$html_wrap = 'İleti metnini en fazla karekter uzunluğu (x) - Yeni satır başı:'; 
+$html_wrap_none = 'Otomatik satır başı olmasın';
+$html_usenet_separator = 'İmzanın önüne Usenet ayırıcısı ekle ("-- \n")'; 
+$html_mark_as = 'Olarak seç:';
+$html_read = 'Okundu'; 
+$html_unread = 'Okunmadı';
+$html_encoding = 'Dil Karekter Karşılığı (encoding)';
 
 // Contacts manager
-$html_add = 'Add';  //to translate
-$html_contacts = 'Contacts';  //to translate
-$html_modify = 'Modify';  //to translate
-$html_back = 'Back';  //to translate
-$html_contact_add = 'Add new contact';  //to translate
-$html_contact_mod = 'Modify a contact';  //to translate
-$html_contact_first = 'First name';  //to translate
-$html_contact_last = 'Last Name';  //to translate
-$html_contact_nick = 'Nick';  //to translate
-$html_contact_mail = 'Mail';  //to translate
-$html_contact_list = 'Contact list of ';  //to translate
-$html_contact_del = 'from the contact list';  //to translate
+$html_add = 'Ekle';  
+$html_contacts = 'Kişiler';
+$html_modify = 'Değiştir';
+$html_back = 'Geri';
+$html_contact_add = 'Yeni kişi ekle'; 
+$html_contact_mod = 'Kişi bilgilerini düzenle';
+$html_contact_first = 'Adı';  
+$html_contact_last = 'Soyadı';
+$html_contact_nick = 'Rumuz';
+$html_contact_mail = 'E-Mail';
+$html_contact_list = 'Kişi listesine mensup';
+$html_contact_del = 'Kişi listesinden'; 
 
-$html_contact_err1 = 'Maximal number of contact is ';  //to translate
-$html_contact_err2 = 'You can\'t add a new contact';  //to translate
-$html_contact_err3 = 'You don\'t have access rights to contact list'; //to translate
-$html_del_msg = 'Delete selected messages ?';  //to translate
-$html_down_mail = 'Download';  //to translate
+$html_contact_err1 = 'En fazla kişi sayısı ';
+$html_contact_err2 = 'Yeni kişi ekleyemzsiniz'; 
+$html_contact_err3 = 'Kişiler listesini düzenleme hakkınız yoktur'; 
+$html_del_msg = 'Seçilen iletiler gerçekten silinsin mi?'; 
+$html_down_mail = 'İndir';  
 
-$original_msg = '-- Orijinal Mesaj --';
+$original_msg = '-- Orijinal İleti --';
 $to_empty = ' \'Alıcı\' alanı boş geçilemez !';
 
 // Images warning
-$html_images_warning = 'For your security, remote pictures are not displayed.'; // to translate
-$html_images_display = 'Display pictures'; // to translate
+$html_images_warning = 'Güvenliğiniz için bazı resimlerin gösterimi engellenmiştir.'; 
+$html_images_display = 'Resimleri göster'; 
 
 // SMTP problems (class_smtp.php)
-$html_smtp_error_no_conn = 'Unable to open SMTP connection';  //to translate
-$html_smtp_error_unexpected = 'Unexpected SMTP response:';  //to translate
+$html_smtp_error_no_conn = 'SMTP serverine erişmede hata oluştu.';  
+$html_smtp_error_unexpected = 'Geçersiz SMTP cevabı geldi:';  
 
 // IMAP messages (class_local.php)
-$lang_could_not_connect = 'Could not connect to server';  //to translate
-$lang_invalid_msg_num = 'Bad Message Number';  //to translate
+$lang_could_not_connect = 'Servere bağlanmada bir hata oluştu';  
+$lang_invalid_msg_num = 'Yanlış Haber Numarası';
 
-$html_file_upload_attack = 'Possible file upload attack';  //to translate
-$html_invalid_email_address = 'Invalid e-mail address';  //to translate
-$html_invalid_msg_per_page = 'Invalid number of messages per page';  //to translate
-$html_invalid_wrap_msg = 'Invalid message wrap width';  //to translate
-$html_seperate_msg_win = 'Messages in separate window';  //to translate
+$html_file_upload_attack = 'Yüklenen dosya bulunamadı';
+$html_invalid_email_address = 'Geçersiz E-mail adresi';
+$html_invalid_msg_per_page = 'Sayfa başı yanlış ileti sayısı';  
+$html_invalid_wrap_msg = 'İleit satır başlığı doğru değil';  
+$html_seperate_msg_win = 'İletiyi yeni bir pencerede göster';
 
 // Exceptions
-$html_err_file_contacts = 'Unable to open contacts file for writing.'; //to translate
-$html_session_file_error = 'Unable to open session file for writing.';  //to translate
-$html_login_not_allowed = 'This login is not allowed for connexion.'; //to translate
+$html_err_file_contacts = 'Kişiler list dosyası bulunamadı.'; 
+$html_session_file_error = 'Oturum biliglerini yazma dosyası bulunamadı.';
+$html_login_not_allowed = 'Bu kullanıcının girişi engellenmiştir.';
 ?>
