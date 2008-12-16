61401cdb467c4335ed16e86ceeb1746c2d7b2f86
Tim Gerundt <tim@gerundt.de>
2008-12-16 22:05:51 +0000

    Add own config file for Fckeditor (and test windows line feeds)

---
+++ config/fckconfig.js
@@ -0,0 +1,32 @@
+﻿/*
+ * This file overwrites the default config from FCKeditor.
+ */
+
+FCKConfig.ToolbarSets["NOCC"] = [
+	['Source','Preview'],
+	['Cut','Copy','Paste','PasteText','PasteWord'],
+	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
+	'/',
+	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
+	['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+	['Link','Unlink','Anchor'],
+	['Table','Rule','SpecialChar','PageBreak'],
+	'/',
+	['Style','FontFormat','FontName','FontSize'],
+	['TextColor','BGColor'],
+	['FitWindow','ShowBlocks','-','About']		// No comma for the last row.
+] ;
+
+FCKConfig.LinkBrowser = false ;
+
+FCKConfig.ImageBrowser = false ;
+
+FCKConfig.FlashBrowser = false ;
+
+FCKConfig.LinkUpload = false ;
+
+FCKConfig.ImageUpload = false ;
+
+FCKConfig.FlashUpload = false ;
