b77219eeb87636bd781824f36e2634a90dd23638
Tobias Mathes <tobias.mathes@gmail.com>
2009-04-04 04:42:12 +0000

    Fix removes .svn folder from theme selection

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ require ('./html/header.php');
                     $handle = opendir('./themes');
                     while (($file = readdir($handle)) != false) 
                     {
-                      if (($file != '.') && ($file != '..'))
+                      if (($file != '.') && ($file != '..') && ($file != '.svn'))
                       {
                         echo '<option value="'.$file.'"';
                         if ($file == $_SESSION['nocc_theme'])
