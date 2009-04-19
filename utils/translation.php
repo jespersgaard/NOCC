20bb452a114fa26df1c3e8727022bfd8b56cea42
Tobias Mathes <tobias.mathes@gmail.com>
2009-04-19 09:31:57 +0000

    changed header description

--- utils/translation.php
+++ utils/translation.php
@@ -1,14 +1,6 @@
 <?php
 /**
- * Sends HTTP headers to forbid transparent proxies, HTTP/1.x proxies to
- * to cache answers from the server running NOCC.
- *
- * This is quite aggressive, we could have set Cache-control to private
- * to forbid only proxy to cache answers but this would allow browser
- * to be able to cache answers; given that some people use NOCC from
- * a public computer, Cache-control is set to no-cache to prevent any
- * caching. This might lower NOCC speed but it's hard to be both secure
- * and cache-friendly when dealing with dynamic content.
+ * Adds additional string formatting for better i18n support
  *
  * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
  * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
@@ -20,7 +12,7 @@
  * @package    NOCC
  * @subpackage Utilities
  * @license    http://www.gnu.org/licenses/ GNU General Public License
- * @version    SVN: $Id: translation.php 1689 2009-04-19 11:29:00Z radical-tobi $
+ * @version    SVN: $Id: translation.php 1637 2009-04-19 11:29:00Z radical-tobi $
  */
 
   function i18n_merge($func_str_translation = NULL, $func_str_insert = NULL, $func_convert = 1) {
