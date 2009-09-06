84f8ef5f51862c8e558c52f6495291bb30576804
Siebrand Mazeland <s.mazeland@xs4all.nl>
2009-09-06 12:28:09 +0000

    Localisation updates from translatewiki.net (2009-09-06)

---
+++ lang/id.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Language configuration file for NOCC
+ *
+ * Copyright 2001 Nicolas Chalanset <nicocha@free.fr>
+ * Copyright 2001 Olivier Cahagne <cahagn_o@epita.fr>
+ *
+ * This file is part of NOCC. NOCC is free software under the terms of the
+ * GNU General Public License. You should have received a copy of the license
+ * along with NOCC.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    NOCC
+ * @subpackage Translations
+ * @license    http://www.gnu.org/licenses/ GNU General Public License
+ * @version    SVN: $Id$
+ */
+
+/** Indonesian (Bahasa Indonesia)
+ *
+ * See the qqq 'language' for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Bennylin
+ * @author Irwangatot
+ */
+
+$default_date_format = '%d-%m-%Y';
+$no_locale_date_format = '%d-%m-%Y';
+$default_time_format = '%I:%M %p';
+$err_user_empty = 'Bagian login kosong';
+$err_passwd_empty = 'Bagian password kosong';
+$alt_delete = 'Hapus pesan terpilih';
+$alt_delete_one = 'Hapus pesan';
+$alt_new_msg = 'Pesan baru';
+$alt_reply = 'Balasan ke penulis';
+$alt_reply_all = 'Balas semua';
+$alt_forward = 'Teruskan';
+$alt_next = 'Selanjutnya';
+$alt_prev = 'Sebelumnya';
+$title_next_page = 'Halaman selanjutnya';
+$title_prev_page = 'Halaman sebelumnya';
+$title_next_msg = 'Pesan berikutnya';
+$title_prev_msg = 'Pesan sebelumnya';
+$html_theme_label = 'Tema:';
+$html_welcome = 'Selamat datang di %1$s';
+$html_login = 'Masuk log';
+$html_passwd = 'Kata sandi';
+$html_submit = 'Kirim';
+$html_help = 'Bantuan';
+$html_server = 'Server';
+$html_wrong = 'Pengguna atau kata sandi salah';
+$html_retry = 'Coba lagi';
+$html_remember = 'Ingat pengaturan';
+$html_lang_label = 'Bahasa:';
+$html_msgperpage_label = 'Pesan perhalaman:';
+$html_preferences = 'Preferensi';
+$html_full_name_label = 'Nama lengkap:';
+$html_email_address_label = 'Alamat surat-e:';
+$html_ccself = 'Tembusan sendiri';
+$html_hide_addresses = 'Sembunyikan alamat';
+$html_outlook_quoting = 'Gaya kutipan-Qutlook';
+$html_reply_to = 'Balas ke';
+$html_use_signature = 'Gunakan tandatangan';
+$html_signature = 'Tanda tangan';
+$html_signature_label = 'Tanda tangan:';
+$html_reply_leadin_label = 'Balasan diawali:';
+$html_prefs_updated = 'Preferensi diperbarui';
+$html_colored_quotes = 'Kutipan berwarna';
+$html_display_struct = 'Tampilan teks tersetruktur';
+$html_folder_subscribe = 'Berlangganan ke';
+$html_folder_rename = 'Ubah nama';
+$html_folder_remove = 'Berhenti berlangganan dari';
+$html_folder_delete = 'Hapus';
+$html_folder_to = 'Kepada';
+$html_filter_remove = 'Hapus';
+$html_filter_body = 'Badan pesan';
+$html_filter_subject = 'Subyek pesan';
+$html_filter_contains = 'berisi';
+$html_filter_moveto = 'Pindah ke';
+$html_select_one = '--Pilih salahsatu--';
+$html_and = 'Dan';
+$html_new_msg_in = 'Pesan baru pada';
+$html_or = 'atau';
+$html_move = 'Pindahkan';
+$html_copy = 'Salin';
+$html_messages_to = 'Pesan pilihan ke';
+$html_gotopage = 'Ke Halaman';
+$html_page = 'Halaman';
+$html_of = 'dari';
+$html_new_msg = 'Tulis';
+$html_reply = 'Balas';
+$html_reply_short = 'Bl:';
+$html_reply_all = 'Balas semua';
+$html_forward_short = 'Lnj:';
+$html_delete = 'Hapus';
+$html_new = 'Baru';
+$html_mark = 'Hapus';
+$html_att_label = 'Lampiran:';
+$html_atts_label_label = 'Lampiran:';
+$html_att_unknown = '[tidak diketahui]';
+$html_attach = 'Terlampir';
+$html_attach_delete = 'Buang Pilihan';
+$html_attach_none = 'Anda harus memilih sebuah berkas untuk dilampirkan!';
+$html_sort_by = 'Urut berdasarkan';
+$html_sort = 'Urutkan';
+$html_from = 'Dari';
+$html_from_label = 'Dari:';
+$html_subject = 'Perihal';
+$html_subject_label = 'Perihal:';
+$html_date = 'Tanggal';
+$html_date_label = 'Tanggal:';
+$html_sent = 'Kirim:';
+$html_wrote = 'tulis';
+$html_size = 'Ukuran';
+$html_totalsize = 'Jumlah Ukuran';
+$html_kb = 'kB';
+$html_mb = 'MB';
+$html_gb = 'GB';
+$html_bytes = 'bytes';
+$html_filename = 'Nama berkas';
+$html_send = 'Kirim';
+$html_cancel = 'Batalkan';
+$html_select = 'Pilih';
+$html_add = 'Tambahkan';
+$html_contacts = '%1$s kontak';
+$html_modify = 'Ubah';
+$html_back = 'Kembali';
+$html_contact_count = '%1$d kontak';
+$html_del_msg = 'Hapus pesan terpilih?';
+$html_down_mail = 'Unduh';
+$html_search = 'Cari';
