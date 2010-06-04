93169da8de9f6060120b6e858a7fcf8728b3fb4a
Tim Gerundt <tim@gerundt.de>
2010-06-04 12:29:43 +0000

    Change version number to 1.9.0(-dev)

--- common.php
+++ common.php
@@ -44,7 +44,7 @@ require_once './utils/crypt.php';
 require_once './utils/translation.php';
 
 $conf->nocc_name = 'NOCC';
-$conf->nocc_version = '1.8.3';
+$conf->nocc_version = '1.9.0-dev';
 $conf->nocc_url = 'http://nocc.sourceforge.net/';
 
 $pwd_to_encrypt = false;
