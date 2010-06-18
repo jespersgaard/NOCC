a2adb6d84ae88a35bf6ae025aef77ea9f797d969
Tim Gerundt <tim@gerundt.de>
2010-06-18 11:50:48 +0000

    Use public/private/__construct keywords in NOCC_HeaderInfo

--- classes/nocc_headerinfo.php
+++ classes/nocc_headerinfo.php
@@ -15,7 +15,6 @@
 
 /**
  * Wrapping a imap_headerinfo() object
- *
  * @package    NOCC
  */
 class NOCC_HeaderInfo {
@@ -24,51 +23,47 @@ class NOCC_HeaderInfo {
      * @var object
      * @access private
      */
-    var $_headerinfo;
+    private $_headerinfo;
     
     /**
      * Default charset
      * @var string
      * @access private
      */
-    var $_defaultcharset;
+    private $_defaultcharset;
     
     /**
      * Initialize the wrapper
-     *
      * @param object $headerinfo imap_headerinfo() object
      * @param string $defaultcharset Default charset
      * @todo Throw exception, if no vaild header info? 
      */
-    function NOCC_HeaderInfo($headerinfo, $defaultcharset = 'ISO-8859-1') {
+    public function __construct($headerinfo, $defaultcharset = 'ISO-8859-1') {
         $this->_headerinfo = $headerinfo;
         $this->_defaultcharset = $defaultcharset;
     }
     
     /**
      * Get the default charset
-     *
      * @return string Default charset
      */
-    function getDefaultCharset() {
+    public function getDefaultCharset() {
         return $this->_defaultcharset;
     }
     
     /**
      * Set the default charset
-     *
      * @param string $defaultcharset Default charset
      */
-    function setDefaultCharset($defaultcharset) {
+    public function setDefaultCharset($defaultcharset) {
         $this->_defaultcharset = $defaultcharset;
     }
     
     /**
      * Get the message id from the header info
-     *
      * @return string Message id
      */
-    function getMessageId() {
+    public function getMessageId() {
         if (isset($this->_headerinfo->message_id)) {
             return $this->_headerinfo->message_id;
         }
@@ -76,11 +71,10 @@ class NOCC_HeaderInfo {
     }
     
     /**
-     * Get the subject from the header info
-     *
+     * Get the subject from the header info8
      * @return string Subject
      */
-    function getSubject() {
+    public function getSubject() {
         if (isset($this->_headerinfo->subject)) {
             return $this->_decodeMimeHeader($this->_headerinfo->subject, $this->_defaultcharset);
         }
@@ -89,10 +83,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Get the "From" address from the header info
-     *
      * @return string "From" address
      */
-    function getFromAddress() {
+    public function getFromAddress() {
         if (isset($this->_headerinfo->fromaddress)) {
             return $this->_decodeMimeHeader($this->_headerinfo->fromaddress, $this->_defaultcharset);
         }
@@ -101,10 +94,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Get the "To" address from the header info
-     *
      * @return string "To" address
      */
-    function getToAddress() {
+    public function getToAddress() {
         if (isset($this->_headerinfo->toaddress)) {
             return $this->_decodeMimeHeader($this->_headerinfo->toaddress, $this->_defaultcharset);
         }
@@ -113,10 +105,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Get the "Cc" address from the header info
-     *
      * @return string "Cc" address
      */
-    function getCcAddress() {
+    public function getCcAddress() {
         if (isset($this->_headerinfo->ccaddress)) {
             return $this->_decodeMimeHeader($this->_headerinfo->ccaddress, $this->_defaultcharset);
         }
@@ -125,10 +116,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Get the "Reply-To" address from the header info
-     *
      * @return string "Reply-To" address
      */
-    function getReplyToAddress() {
+    public function getReplyToAddress() {
         if (isset($this->_headerinfo->reply_toaddress)) {
             return $this->_decodeMimeHeader($this->_headerinfo->reply_toaddress, $this->_defaultcharset);
         }
@@ -137,10 +127,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Get the date (in Unix time) from the header info
-     *
      * @return int Date in Unix time
      */
-    function getTimestamp() {
+    public function getTimestamp() {
         if (isset($this->_headerinfo->udate)) {
             return $this->_headerinfo->udate;
         }
@@ -149,10 +138,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Is the mail unread?
-     *
      * @return boolean Is unread?
      */
-    function isUnread() {
+    public function isUnread() {
         if (($this->_headerinfo->Unseen == 'U') || ($this->_headerinfo->Recent == 'N')) {
             return true;
         }
@@ -161,10 +149,9 @@ class NOCC_HeaderInfo {
     
     /**
      * Is the mail flagged?
-     *
      * @return boolean Is flagged?
      */
-    function isFlagged() {
+    public function isFlagged() {
         if ($this->_headerinfo->Flagged == 'F') {
             return true;
         }
@@ -173,13 +160,12 @@ class NOCC_HeaderInfo {
     
     /**
      * Decode MIME header
-     *
      * @param string $mimeheader Encoded MIME header
      * @param string $defaultcharset Default charset
      * @return string Decoded MIME header
      * @access private
      */
-    function _decodeMimeHeader($mimeheader, $defaultcharset) {
+    private function _decodeMimeHeader($mimeheader, $defaultcharset) {
         $decodedheader = '';
         if (isset($mimeheader)) {
             $mimeheader = str_replace('x-unknown', $defaultcharset, $mimeheader);
