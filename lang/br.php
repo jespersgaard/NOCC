80910449dea8f6c62d8654dbe99d621d41d0920a
Siebrand Mazeland <s.mazeland@xs4all.nl>
2010-08-29 17:58:14 +0000

    Localisation updates from http://translatewiki.net

--- lang/br.php
+++ lang/br.php
@@ -15,7 +15,7 @@
  * @version    SVN: $Id$
  */
 /** Breton (Brezhoneg)
- *
+ * 
  * See the qqq 'language' for message documentation incl. usage of parameters
  * To improve a translation please visit http://translatewiki.net
  *
