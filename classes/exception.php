3d3baa3ec6d87d99dec128051c13166d88d382d8
Tim Gerundt <tim@gerundt.de>
2011-02-06 12:05:47 +0000

    Rewrite class NoccException to PHP5

--- classes/exception.php
+++ classes/exception.php
@@ -1,27 +1,39 @@
 <?php
-
-/*
+/**
  * Very simplistic version of PEAR.php
  */
-class NoccException
-{
-    var $message = '';
+class NoccException {
+    /**
+     * Message
+     * @var string
+     */
+    private $message = '';
 
-    function isException($data) {
+    /**
+     * Initialize the exception
+     * @param string $message Message
+     */
+    public function __construct($message = 'unknown error') {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the message from the exception
+     * @return string Message
+     */
+    public function getMessage() {
+        return $this->message;
+    }
+
+    /**
+     * ...
+     * @param object $data Data
+     * @return boolean Is exception?
+     */
+    public static function isException($data) {
         return (bool)(is_object($data) &&
                       ((get_class($data) == "NoccException") ||
                (get_class($data) == "noccexception")));
     }
-
-    function NoccException($message = "unknown error")
-    {
-        $this->message = $message;
-    }
-
-    function getMessage ()
-    {
-        return ($this->message);
-    }
 }
-
 ?>
