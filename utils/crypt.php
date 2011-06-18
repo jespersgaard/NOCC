1f3679c71062d2174058c5d662ce33a1b94f38cf
Tim Gerundt <tim@gerundt.de>
2011-06-18 14:05:11 +0000

    Cleanup crypt.php

--- utils/crypt.php
+++ utils/crypt.php
@@ -35,67 +35,32 @@ function decr($string, $key) {
     return $string;
 }
 
-/*
- The genkey function is just an example.
- It would have to be modified in order to generate a proper key that fits the
- requirements of the encr/decr functions that will be used.
- It should also be extended, to somehow test if cookies are supported by the
- browser.
- If not, NOCC should override the settings (, maybe inform the user) and don't
- do any encryption.
- I think genkey must be run before any other HTML or headers. This seems to be a requirement for the cookie to be sent properly.
-*/
-function genkey($keylength, $cookiename) {
-// Generates a random key of a specified length, and sends
-//it as a cookie.
-// Returns the key, though I don't see a need in doing that.
-
-  $rkey = substr(preg_replace("|[^A-Za-z0-9]|",
-                  "",
-                  crypt(time())) .
-         preg_replace("|[^A-Za-z0-9]|",
-                  "",
-                  crypt(time())) .
-         preg_replace("[|^A-Za-z0-9]|",
-                  "",
-                  crypt(time())),
-         0, $keylength);
-
-  setcookie($cookiename, $rkey, time() + 3600);
-
-  return $rkey;
-}
-
+/**
+ * Returns encrypted password
+ * @param string $passwd Password
+ * @param string $rkey Master key
+ * @return string Encrypted password
+ */
 function encpass($passwd, $rkey) {
-  // Encrypts $passwd with $key and returns encrypted password
-
-  //$encpasswd = encr(base64_encode($passwd),$rkey);
   $encpasswd = encr(bin2hex($passwd), $rkey);
-  //$encpasswd = encr($passwd,$rkey);
+  
   return $encpasswd;
 }
 
+/**
+ * Returns decrypted password
+ * @param string $cipher Cipher
+ * @param string $rkey Master key
+ * @return string Decrypted password
+ */
 function decpass($cipher, $rkey) {
-  // Decrypts $cipher returns decrypted password.
-
-  //$decpasswd = base64_decode(decr($cipher, $rkey));
   $dechexpasswd = decr($cipher, $rkey);
-  $decpasswd = '';
   
+  $decpasswd = '';
   for ($i=0; $i<strlen($dechexpasswd)/2; $i++) {
     $decpasswd .= chr(base_convert(substr($dechexpasswd, $i * 2, 2), 16, 10));
   }
-  //$decpasswd = decr($cipher, $rkey);
+  
   return $decpasswd;
 }
-
-
-// I think also delkey must be run before any other HTML or
-//headers.
-
-function delkey($cookie) {
-  // Deletes the cookie
-  setcookie($cookie, "", time() - 3600);
-}
-
 ?>
